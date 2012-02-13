fa3e125fb597753e7948ed8fe7bf5ad7f2d80f1e
NickAVV <moldorma@gmail.com>
2012-02-13 14:57:02 -0500

    Adding RAAC

---
+++ RAAC/src/edu/uml/cs/raac/Isense.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (c) 2009, iSENSE Project. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials
+ * provided with the distribution. Neither the name of the University of
+ * Massachusetts Lowell nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+package edu.uml.cs.raac;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import edu.uml.cs.raac.pincushion.BluetoothService;
+import edu.uml.cs.raac.pincushion.pinpointInterface;
+
+public class Isense extends Activity implements OnClickListener {
+	Button testBtn, serialBtn, rcrdBtn;
+	TextView testResult;
+	TextView mConnected;
+	static pinpointInterface ppi;
+	private String mConnectedDeviceName = null;
+	private BluetoothService mChatService = null;
+	private BluetoothAdapter mBluetoothAdapter = null;
+	private ArrayList<String[]> data;
+
+	// Intent request codes
+	private static final int REQUEST_CONNECT_DEVICE = 1;
+	private static final int REQUEST_ENABLE_BT = 4;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+		testBtn = (Button) findViewById(R.id.btn_selectppt);
+		serialBtn = (Button) findViewById(R.id.btn_getSerial);
+		rcrdBtn = (Button) findViewById(R.id.btn_getRcrd);
+		testResult = (TextView) findViewById(R.id.resultText);
+		mConnected = (TextView) findViewById(R.id.title_connected_to);
+
+		if (mBluetoothAdapter == null) {
+			testResult.setText("Bluetooth is not available on this device");
+		} else {
+			if (!mBluetoothAdapter.isEnabled()) {
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+			}
+		}
+
+		testBtn.setOnClickListener(this);
+		serialBtn.setOnClickListener(this);
+		serialBtn.setEnabled(false);
+		rcrdBtn.setOnClickListener(this);
+		rcrdBtn.setEnabled(false);
+
+		mChatService = new BluetoothService(this, mHandler);
+	}
+
+	@Override
+	public synchronized void onResume() {
+		super.onResume();
+		// Performing this check in onResume() covers the case in which BT was
+		// not enabled during onStart(), so we were paused to enable it...
+		// onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+		if (mChatService != null) {
+			// Only if the state is STATE_NONE, do we know that we haven't started already
+			if (mChatService.getState() == BluetoothService.STATE_NONE) {
+				// Start the Bluetooth chat services
+				mChatService.start();
+			}
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if(ppi != null)
+			ppi.disconnect();
+	}
+
+	@Override
+	public void onClick(View v) {
+		if( v == testBtn ) {
+			Intent serverIntent = new Intent(this, DeviceListActivity.class);
+			startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+		}
+		if( v == serialBtn ) {
+			testResult.append("\n"+ppi.getSerialNumber());
+			
+		}
+		if( v == rcrdBtn ) {
+			try {
+				data = ppi.getData();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				for (String[] strray : data) {
+					for (String str : strray) {
+						testResult.append("\n"+str);
+					}
+				}
+			} catch (NullPointerException e) {
+				testResult.append("\nNo data read, please try again.");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// The Handler that gets information back from the BluetoothChatService
+	private final Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case BluetoothService.MESSAGE_STATE_CHANGE:
+				switch (msg.arg1) {
+				case BluetoothService.STATE_CONNECTED:
+					mConnected.setText("");
+					mConnected.append(mConnectedDeviceName);
+					ppi = new pinpointInterface(mChatService);
+					serialBtn.setEnabled(true);
+					rcrdBtn.setEnabled(true);
+					break;
+				case BluetoothService.STATE_CONNECTING:
+					mConnected.setText("Connecting...");
+					break;
+				case BluetoothService.STATE_LISTEN:
+				case BluetoothService.STATE_NONE:
+					mConnected.setText("Not connected");
+					break;
+				}
+				break;
+			case BluetoothService.MESSAGE_WRITE:
+			case BluetoothService.MESSAGE_READ:
+				byte[] readBuf = (byte[]) msg.obj;
+				String readMessage = new String(readBuf, 0, msg.arg1);
+				testResult.setText(readMessage);
+				break;
+			case BluetoothService.MESSAGE_DEVICE_NAME:
+				mConnectedDeviceName = msg.getData().getString("device_name");
+				break;
+			case BluetoothService.MESSAGE_TOAST:
+			}
+		}
+	};
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case REQUEST_CONNECT_DEVICE:
+			// When DeviceListActivity returns with a device to connect
+			if (resultCode == Activity.RESULT_OK) {
+				// Get the device MAC address
+				String address = data.getExtras()
+						.getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+				// Get the BLuetoothDevice object
+				BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+				// Attempt to connect to the device
+				mChatService.connect(device);
+			}
+			break;
+		case REQUEST_ENABLE_BT:
+			// When the request to enable Bluetooth returns
+			if (resultCode == Activity.RESULT_OK) {
+				// Bluetooth is now enabled, so set up a chat session
+			} else {
+				// User did not enable Bluetooth or an error occured
+				Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+				finish();
+			}
+		}
+	}
+}
