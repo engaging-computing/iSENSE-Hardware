c9408a0f1ece3bb57971b9b08507eec8567c475a
Michael Stowell <StowellMichael@gmail.com>
2012-06-05 11:47:23 -0400

    Completed River Walk v3, featuring merely 1 button functionality (now on iSENSE, not dev).

--- River_Walk_App/src/edu/uml/cs/isense/picture/pictures.java
+++ River_Walk_App/src/edu/uml/cs/isense/picture/pictures.java
@@ -78,14 +78,14 @@ public class pictures extends Activity implements LocationListener {
 	
 	RestAPI rapi;
 	
-	//private static final String loginName = "RiverWalk App User";
-	//private static final String loginPass = "nexusWINS";
-	//private static final String experimentNum = "424";
+	private static final String loginName = "RiverWalk App User";
+	private static final String loginPass = "nexusWINS";
+	private static final String experimentNum = "424";
 	//real iSENSE
 	
-	private static final String loginName = "RiverWalker";
-	private static final String loginPass = "SimonKit";
-	private static final String experimentNum = "347";
+	//private static final String loginName = "RiverWalker";
+	//private static final String loginPass = "SimonKit";
+	//private static final String experimentNum = "347";
 	//dev
 	
 	private static boolean gpsWorking = false;
