2e8decf96d63f293031ea1d9d901170a48a7f99b
Jeremy Poulin <jaypoulz@gmail.com>
2012-07-03 08:58:17 -0400

    Removed logs in General Physics App

--- General_Physics_App/src/edu/uml/cs/isense/amusement/ExperimentAdapter.java
+++ General_Physics_App/src/edu/uml/cs/isense/amusement/ExperimentAdapter.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import android.content.Context;
 import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,196 +16,172 @@ import edu.uml.cs.isense.objects.ExperimentField;
 
 public class ExperimentAdapter extends ArrayAdapter<Experiment> {
 	@SuppressWarnings("unused")
-		private final int maxDimension = 50;
-    public ArrayList<Experiment> items;
-    private Context mContext; 
-    private int resourceID;
-    private int loadingRow;
-    public int itemsLoaded;
-    public boolean allItemsLoaded;
-    private Boolean loading;
-    private UIUpdateTask updateTask;
-    private Handler uiHandler = new Handler();
-    private RestAPI rapi;
+	private final int maxDimension = 50;
+	public ArrayList<Experiment> items;
+	private Context mContext;
+	private int resourceID;
+	private int loadingRow;
+	public int itemsLoaded;
+	public boolean allItemsLoaded;
+	private Boolean loading;
+	private UIUpdateTask updateTask;
+	private Handler uiHandler = new Handler();
+	private RestAPI rapi;
 	public static final int pageSize = 10;
 	public int page = 0;
 	public String action = "browse";
 	public String query = "";
 
-    public ExperimentAdapter(Context context, int textViewResourceId, int loadingRow, ArrayList<Experiment> items) {
-            super(context, textViewResourceId, items);
-            this.items = items;
-            mContext = context;
-            resourceID = textViewResourceId;
-            this.loadingRow = loadingRow;
-            itemsLoaded = 0;
-            allItemsLoaded = false;
-            loading = false;
-            updateTask = new UIUpdateTask();
-            rapi = RestAPI.getInstance();
-    }
-    
-    public int getCount() {
-    	int count = itemsLoaded;
-    	if (!allItemsLoaded) ++count;
-    	return count;
-    }
-    
-    public Experiment getItem(int position) {
-    	Experiment result;
-    	synchronized(items) {
-    		result = items.get(position);
-    	}
-    	return result;
-    }
-    
-    public long getItemId(int position) {
-    	return position;
-    }
+	public ExperimentAdapter(Context context, int textViewResourceId,
+			int loadingRow, ArrayList<Experiment> items) {
+		super(context, textViewResourceId, items);
+		this.items = items;
+		mContext = context;
+		resourceID = textViewResourceId;
+		this.loadingRow = loadingRow;
+		itemsLoaded = 0;
+		allItemsLoaded = false;
+		loading = false;
+		updateTask = new UIUpdateTask();
+		rapi = RestAPI.getInstance();
+	}
 
-    @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {	
-    	boolean isLastRow = position >= itemsLoaded;
-    	View v = convertView;
-        LayoutInflater vi = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        
-    	if (isLastRow) {
-        	v = vi.inflate(loadingRow, null);
-    	} else if (v == null || v.findViewById(R.id.toptext) == null) {
-    		v = vi.inflate(resourceID, null);
-    	} 
+	public int getCount() {
+		int count = itemsLoaded;
+		if (!allItemsLoaded)
+			++count;
+		return count;
+	}
 
-    	if (!isLastRow && items.size() != 0) {
-    		Experiment e = items.get(position);
-            if (e != null) {
-                TextView tt = (TextView) v.findViewById(R.id.toptext);
-                TextView bt = (TextView) v.findViewById(R.id.bottomtext);
-                if(tt != null) {
-                    tt.setText(e.name);                            
-                }
-                if(bt != null) {
-                	if (e.firstname == "" && e.lastname == "") {
-                		bt.setVisibility(View.GONE);
-                	}
-                	else {
-                		bt.setText("Created By: " + e.firstname + " " + e.lastname);
-                	}
-                }
-            }
-    	} else {
-    		if (!allItemsLoaded) {
-    			page++;
-    			synchronized( loading ) {
-    				if( !loading.booleanValue() ) {
-    					loading = Boolean.TRUE;
-    					Thread t = new LoadingThread();
-    					t.start();
-    				}
-    			}
-    		} else {
-    			uiHandler.post( updateTask );
-    		}
-    	}
-        return v;
-    }
-    
-    class LoadingThread extends Thread {
+	public Experiment getItem(int position) {
+		Experiment result;
+		synchronized (items) {
+			result = items.get(position);
+		}
+		return result;
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		boolean isLastRow = position >= itemsLoaded;
+		View v = convertView;
+		LayoutInflater vi = (LayoutInflater) mContext
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		if (isLastRow) {
+			v = vi.inflate(loadingRow, null);
+		} else if (v == null || v.findViewById(R.id.toptext) == null) {
+			v = vi.inflate(resourceID, null);
+		}
+
+		if (!isLastRow && items.size() != 0) {
+			Experiment e = items.get(position);
+			if (e != null) {
+				TextView tt = (TextView) v.findViewById(R.id.toptext);
+				TextView bt = (TextView) v.findViewById(R.id.bottomtext);
+				if (tt != null) {
+					tt.setText(e.name);
+				}
+				if (bt != null) {
+					if (e.firstname == "" && e.lastname == "") {
+						bt.setVisibility(View.GONE);
+					} else {
+						bt.setText("Created By: " + e.firstname + " "
+								+ e.lastname);
+					}
+				}
+			}
+		} else {
+			if (!allItemsLoaded) {
+				page++;
+				synchronized (loading) {
+					if (!loading.booleanValue()) {
+						loading = Boolean.TRUE;
+						Thread t = new LoadingThread();
+						t.start();
+					}
+				}
+			} else {
+				uiHandler.post(updateTask);
+			}
+		}
+		return v;
+	}
+
+	class LoadingThread extends Thread {
 		public void run() {
-			ExpLoaded expPair = rapi.getAllExperiments(page, pageSize, action, query);
-			//ArrayList<Experiment> new_items = rapi.getExperiments(page,
-				//	pageSize, action, query);
-			ArrayList<Experiment> selected_items = new ArrayList<Experiment>();
-			boolean logsOn = false;
-			for (Experiment exp : expPair.exp) {
-				
-				Log.wtf("exp #", exp.experiment_id + "");
-				if(exp.experiment_id == 422 || exp.experiment_id == 427)
-					logsOn = true;
-				else
-					logsOn = false;
+			ExpLoaded expPair = rapi.getAllExperiments(page, pageSize, action,
+					query);
+			// ArrayList<Experiment> new_items = rapi.getExperiments(page,
+			// pageSize, action, query);
 
+			ArrayList<Experiment> selected_items = new ArrayList<Experiment>();
+			for (Experiment exp : expPair.exp) {
 				ArrayList<ExperimentField> exp_fields = rapi
 						.getExperimentFields(exp.experiment_id);
 				int fieldNum = 1;
 				boolean pass = true;
-				if(exp_fields.isEmpty()) pass = false;
+				if (exp_fields.isEmpty())
+					pass = false;
 				for (ExperimentField field : exp_fields) {
 
 					switch (fieldNum) {
 					// Accel-x
 					case 1:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 25");
 						if (field.type_id != 25)
 							pass = false;
 						break;
 					// Accel-y
 					case 2:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 25");
 						if (field.type_id != 25)
 							pass = false;
 						break;
 					// Accel-z
 					case 3:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 25");
 						if (field.type_id != 25)
 							pass = false;
 						break;
 					// Accel-Total
 					case 4:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 25");
 						if (field.type_id != 25)
 							pass = false;
 						break;
 					// Latitude
 					case 5:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 19");
 						if (field.type_id != 19)
 							pass = false;
 						break;
 					// Longitude
 					case 6:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 19");
 						if (field.type_id != 19)
 							pass = false;
 						break;
 					// Heading
 					case 7:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 10");
 						if (field.type_id != 10)
 							pass = false;
 						break;
 					// Mag-x
 					case 8:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 21 or 22");
 						if (field.type_id != 21 && field.type_id != 22)
 							pass = false;
 						break;
 					// Mag-y
 					case 9:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 21 or 22");
 						if (field.type_id != 21 && field.type_id != 22)
 							pass = false;
 						break;
 					// Mag-z
 					case 10:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 21 or 22");
 						if (field.type_id != 21 && field.type_id != 22)
 							pass = false;
 						break;
 					// Time
 					case 11:
-						if(logsOn)
-							Log.e("Fields", "Is: " + field.type_id + ", but should be: 7");
 						if (field.type_id != 7)
 							pass = false;
 						break;
@@ -215,14 +190,14 @@ public class ExperimentAdapter extends ArrayAdapter<Experiment> {
 						break;
 					}
 					fieldNum++;
-					
+
 					if (!pass)
 						break;
 				}
 				if (pass)
 					selected_items.add(exp);
 			}
-	
+
 			if (expPair.getLoaded(expPair)) {
 				allItemsLoaded = true;
 			} else {
@@ -239,11 +214,11 @@ public class ExperimentAdapter extends ArrayAdapter<Experiment> {
 			uiHandler.post(updateTask);
 		}
 	}
-    
-    class UIUpdateTask implements Runnable {
-        public void run() {
-            notifyDataSetChanged();
-        }
-    }
-    
+
+	class UIUpdateTask implements Runnable {
+		public void run() {
+			notifyDataSetChanged();
+		}
+	}
+
 }
