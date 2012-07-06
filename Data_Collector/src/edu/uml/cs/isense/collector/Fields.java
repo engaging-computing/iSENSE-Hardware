52fefcbd1a0ec2465817a2df4a31bb467d0b40d8
Michael Stowell <StowellMichael@gmail.com>
2012-07-06 10:18:34 -0400

    Uploading possible (on some experiments) regardless of order of fields.  Still needs to include all potential fields.

---
+++ Data_Collector/src/edu/uml/cs/isense/collector/Fields.java
@@ -0,0 +1,16 @@
+package edu.uml.cs.isense.collector;
+
+public class Fields {
+	public String accel_x, accel_y, accel_z, accel_total;
+	public double temperature;
+	public long   timeMillis;
+	public double lux;
+	public String angle_deg, angle_rad;
+	public double latitude, longitude;
+	public double mag_x, mag_y, mag_z, mag_total;
+	public double altitude;
+	public double pressure;
+	
+	public Fields() {
+	}
+}
