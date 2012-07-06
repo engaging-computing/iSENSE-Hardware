4f35aaeb928d8d9d46e4160a66256539ad3a103b
Jeremy Poulin <jaypoulz@gmail.com>
2012-07-06 14:01:30 -0400

    Finished sorting fields taken and attempting to add sensors like pressure and ambient temperature

--- Data_Collector/src/edu/uml/cs/isense/collector/Fields.java
+++ Data_Collector/src/edu/uml/cs/isense/collector/Fields.java
@@ -2,14 +2,14 @@ package edu.uml.cs.isense.collector;
 
 public class Fields {
 	public String accel_x, accel_y, accel_z, accel_total;
-	public double temperature;
+	public String temperature;
 	public long   timeMillis;
-	public double lux;
+	public String lux;
 	public String angle_deg, angle_rad;
 	public double latitude, longitude;
 	public double mag_x, mag_y, mag_z, mag_total;
-	public double altitude;
-	public double pressure;
+	public String altitude;
+	public String pressure;
 	
 	public Fields() {
 	}
