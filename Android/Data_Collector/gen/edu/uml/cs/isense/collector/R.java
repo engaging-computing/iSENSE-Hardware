b09bad9bea471efd0e7696588599c1206865f133
Jeremy Poulin <jaypoulz@gmail.com>
2012-07-09 14:21:01 -0400

    Fixed checkboxes

--- Android/Data_Collector/gen/edu/uml/cs/isense/collector/R.java
+++ Android/Data_Collector/gen/edu/uml/cs/isense/collector/R.java
@@ -32,53 +32,57 @@ public final class R {
         public static final int spinner_black_16=0x7f02000e;
     }
     public static final class id {
-        public static final int BrowseButton=0x7f07001b;
-        public static final int ExperimentInput=0x7f07001a;
-        public static final int ExperimentLabel=0x7f070019;
-        public static final int ImageViewLogo=0x7f07000c;
-        public static final int aboutSync=0x7f070028;
-        public static final int bottomLayout=0x7f070027;
-        public static final int bottomtext=0x7f070004;
-        public static final int buttonGoBack=0x7f07002b;
-        public static final int iSenseOnlyOptions=0x7f070016;
-        public static final int icon=0x7f070002;
-        public static final int image=0x7f07002d;
-        public static final int instructions=0x7f070025;
-        public static final int isCanobie=0x7f07001f;
-        public static final int itemContent=0x7f070001;
-        public static final int layout_root=0x7f070006;
-        public static final int loginInfo=0x7f07000b;
-        public static final int menuPrompt=0x7f070011;
-        public static final int middleLayout=0x7f070026;
-        public static final int nameLabel=0x7f070017;
-        public static final int otherOptions=0x7f07001c;
-        public static final int passwordInput=0x7f07000a;
-        public static final int passwordLabel=0x7f070009;
-        public static final int pictureButton=0x7f070022;
-        public static final int pictureCount=0x7f070012;
-        public static final int receiveButton=0x7f07002a;
-        public static final int rideNameLabel=0x7f070020;
-        public static final int ridename=0x7f07000d;
-        public static final int rides=0x7f070021;
-        public static final int sendButton=0x7f070029;
-        public static final int sensorlabel=0x7f070013;
-        public static final int sensorscrollview=0x7f070000;
-        public static final int sessionName=0x7f070018;
-        public static final int setupScroll=0x7f070015;
-        public static final int startStop=0x7f070010;
-        public static final int studentLabel=0x7f07001d;
-        public static final int studentNumber=0x7f07001e;
-        public static final int subsensorlabel=0x7f070014;
-        public static final int text=0x7f07002e;
-        public static final int time=0x7f07000f;
-        public static final int toast_layout_root=0x7f07002c;
-        public static final int topLayout=0x7f070024;
-        public static final int toptext=0x7f070003;
-        public static final int trobber=0x7f070005;
-        public static final int usernameInput=0x7f070008;
-        public static final int usernameLabel=0x7f070007;
-        public static final int values=0x7f07000e;
-        public static final int videoButton=0x7f070023;
+        public static final int BrowseButton=0x7f07001f;
+        public static final int ExperimentInput=0x7f07001e;
+        public static final int ExperimentLabel=0x7f07001d;
+        public static final int ImageViewLogo=0x7f07000f;
+        public static final int aboutSync=0x7f07002c;
+        public static final int bottomLayout=0x7f07002b;
+        public static final int bottomtext=0x7f070007;
+        public static final int buttonGoBack=0x7f07002f;
+        public static final int buttons=0x7f070000;
+        public static final int check_layout=0x7f070016;
+        public static final int iSenseOnlyOptions=0x7f07001a;
+        public static final int icon=0x7f070005;
+        public static final int image=0x7f070031;
+        public static final int instructions=0x7f070029;
+        public static final int isCanobie=0x7f070023;
+        public static final int itemContent=0x7f070004;
+        public static final int layout_root=0x7f070009;
+        public static final int loginInfo=0x7f07000e;
+        public static final int menuPrompt=0x7f070014;
+        public static final int middleLayout=0x7f07002a;
+        public static final int nameLabel=0x7f07001b;
+        public static final int otherOptions=0x7f070020;
+        public static final int passwordInput=0x7f07000d;
+        public static final int passwordLabel=0x7f07000c;
+        public static final int pictureButton=0x7f070026;
+        public static final int pictureCount=0x7f070015;
+        public static final int receiveButton=0x7f07002e;
+        public static final int rideNameLabel=0x7f070024;
+        public static final int ridename=0x7f070010;
+        public static final int rides=0x7f070025;
+        public static final int sendButton=0x7f07002d;
+        public static final int sensor_back=0x7f070003;
+        public static final int sensor_ok=0x7f070002;
+        public static final int sensorlabel=0x7f070017;
+        public static final int sensorscrollview=0x7f070001;
+        public static final int sessionName=0x7f07001c;
+        public static final int setupScroll=0x7f070019;
+        public static final int startStop=0x7f070013;
+        public static final int studentLabel=0x7f070021;
+        public static final int studentNumber=0x7f070022;
+        public static final int subsensorlabel=0x7f070018;
+        public static final int text=0x7f070032;
+        public static final int time=0x7f070012;
+        public static final int toast_layout_root=0x7f070030;
+        public static final int topLayout=0x7f070028;
+        public static final int toptext=0x7f070006;
+        public static final int trobber=0x7f070008;
+        public static final int usernameInput=0x7f07000b;
+        public static final int usernameLabel=0x7f07000a;
+        public static final int values=0x7f070011;
+        public static final int videoButton=0x7f070027;
     }
     public static final class layout {
         public static final int choosesensor=0x7f030000;
@@ -96,39 +100,73 @@ public final class R {
         public static final int beep=0x7f040000;
     }
     public static final class string {
+        /**  3  
+         */
         public static final int accel_total=0x7f050005;
+        /**  0  
+         */
         public static final int accel_x=0x7f050002;
+        /**  1  
+         */
         public static final int accel_y=0x7f050003;
+        /**  2  
+         */
         public static final int accel_z=0x7f050004;
+        /**  14 
+         */
         public static final int altitude=0x7f050010;
         public static final int app_name=0x7f050015;
+        public static final int back=0x7f050034;
         public static final int browse=0x7f050027;
         public static final int cancel=0x7f050031;
         public static final int canobieRides=0x7f05002a;
+        /**  16 
+         */
         public static final int compatible=0x7f050012;
         public static final int eula=0x7f05001b;
         public static final int experimentNum=0x7f050026;
         public static final int experiments_no_items=0x7f050019;
+        /**  10 
+         */
         public static final int heading_deg=0x7f05000c;
+        /**  11 
+         */
         public static final int heading_rad=0x7f05000d;
         public static final int hello=0x7f050014;
         public static final int iSENSELogo=0x7f050021;
-        public static final int iSENSElogo=0x7f050033;
+        public static final int iSENSElogo=0x7f050035;
         public static final int iSenseConfig=0x7f050016;
         public static final int incompatible=0x7f050013;
         public static final int instructions=0x7f05002e;
+        /**  4  
+         */
         public static final int latitude=0x7f050006;
+        /**  5  
+         */
         public static final int longitude=0x7f050007;
+        /**  15 
+         */
         public static final int luminous_flux=0x7f050011;
+        /**  9  
+         */
         public static final int magnetic_total=0x7f05000b;
+        /**  6  
+         */
         public static final int magnetic_x=0x7f050008;
+        /**  7  
+         */
         public static final int magnetic_y=0x7f050009;
+        /**  8  
+         */
         public static final int magnetic_z=0x7f05000a;
         public static final int menuDirections=0x7f050023;
         public static final int notLoggedIn=0x7f05001f;
         public static final int null_string=0x7f050000;
+        public static final int okay=0x7f050033;
         public static final int otherConfig=0x7f050017;
         public static final int picAndVidCount=0x7f050022;
+        /**  13 
+         */
         public static final int pressure=0x7f05000f;
         public static final int receive=0x7f050030;
         public static final int ride=0x7f05002b;
@@ -143,6 +181,8 @@ public final class R {
         public static final int syncTime=0x7f050032;
         public static final int takePic=0x7f05002c;
         public static final int takeVid=0x7f05002d;
+        /**  12 
+         */
         public static final int temperature=0x7f05000e;
         public static final int time=0x7f050001;
         public static final int timeElapsed=0x7f050024;
