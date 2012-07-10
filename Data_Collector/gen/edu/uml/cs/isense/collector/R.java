c95f38fb083ba483bcfa6730f7dfae22dc1adf5a
Jeremy Poulin <jaypoulz@gmail.com>
2012-07-10 13:43:23 -0400

    Added booleans for return checks

--- Data_Collector/gen/edu/uml/cs/isense/collector/R.java
+++ Data_Collector/gen/edu/uml/cs/isense/collector/R.java
@@ -12,66 +12,72 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int bluechecksmall=0x7f020001;
-        public static final int canobie_check=0x7f020002;
-        public static final int checkmark=0x7f020003;
-        public static final int colorless=0x7f020004;
-        public static final int ic_menu_login=0x7f020005;
-        public static final int ic_menu_settings=0x7f020006;
-        public static final int ic_menu_synctime=0x7f020007;
-        public static final int ic_menu_upload=0x7f020008;
-        public static final int icon=0x7f020009;
-        public static final int logo=0x7f02000a;
-        public static final int main_background=0x7f02000b;
-        public static final int progress_small=0x7f02000c;
-        public static final int red_x=0x7f02000d;
-        public static final int spinner_black_16=0x7f02000e;
+        public static final int background_running=0x7f020001;
+        public static final int bluechecksmall=0x7f020002;
+        public static final int canobie_check=0x7f020003;
+        public static final int checkmark=0x7f020004;
+        public static final int colorless=0x7f020005;
+        public static final int ic_menu_login=0x7f020006;
+        public static final int ic_menu_settings=0x7f020007;
+        public static final int ic_menu_synctime=0x7f020008;
+        public static final int ic_menu_upload=0x7f020009;
+        public static final int icon=0x7f02000a;
+        public static final int logo=0x7f02000b;
+        public static final int logo_green=0x7f02000c;
+        public static final int logo_red=0x7f02000d;
+        public static final int main_background=0x7f02000e;
+        public static final int progress_small=0x7f02000f;
+        public static final int red_x=0x7f020010;
+        public static final int spinner_black_16=0x7f020011;
+        public static final int toast_background_check=0x7f020012;
+        public static final int toast_background_x=0x7f020013;
     }
     public static final class id {
-        public static final int BrowseButton=0x7f06001e;
-        public static final int ExperimentInput=0x7f06001d;
-        public static final int ExperimentLabel=0x7f06001c;
+        public static final int BrowseButton=0x7f06001f;
+        public static final int ExperimentInput=0x7f06001e;
+        public static final int ExperimentLabel=0x7f06001d;
         public static final int ExperimentSearchInput=0x7f060008;
-        public static final int ImageViewLogo=0x7f06000f;
-        public static final int aboutSync=0x7f060025;
-        public static final int bottomLayout=0x7f060024;
+        public static final int ImageViewLogo=0x7f060010;
+        public static final int aboutSync=0x7f060026;
+        public static final int bottomLayout=0x7f060025;
         public static final int bottomtext=0x7f060007;
-        public static final int buttonGoBack=0x7f060028;
+        public static final int buttonGoBack=0x7f060029;
         public static final int buttons=0x7f060000;
-        public static final int check_layout=0x7f060016;
-        public static final int iSenseOnlyOptions=0x7f06001a;
+        public static final int check_layout=0x7f060017;
+        public static final int iSenseOnlyOptions=0x7f06001b;
         public static final int icon=0x7f060005;
-        public static final int image=0x7f06002a;
-        public static final int instructions=0x7f060022;
+        public static final int image=0x7f06002b;
+        public static final int instructions=0x7f060023;
         public static final int itemContent=0x7f060004;
         public static final int layout_root=0x7f06000a;
-        public static final int loginInfo=0x7f060010;
-        public static final int menuPrompt=0x7f060014;
-        public static final int middleLayout=0x7f060023;
-        public static final int nameLabel=0x7f06001b;
+        public static final int loginInfo=0x7f060011;
+        public static final int mainScreen=0x7f06000f;
+        public static final int menuPrompt=0x7f060015;
+        public static final int middleLayout=0x7f060024;
+        public static final int nameLabel=0x7f06001c;
         public static final int passwordInput=0x7f06000e;
         public static final int passwordLabel=0x7f06000d;
-        public static final int pictureButton=0x7f06001f;
-        public static final int pictureCount=0x7f060015;
-        public static final int receiveButton=0x7f060027;
-        public static final int sendButton=0x7f060026;
+        public static final int pictureButton=0x7f060020;
+        public static final int pictureCount=0x7f060016;
+        public static final int receiveButton=0x7f060028;
+        public static final int sendButton=0x7f060027;
         public static final int sensor_back=0x7f060003;
         public static final int sensor_ok=0x7f060002;
-        public static final int sensorlabel=0x7f060017;
+        public static final int sensorlabel=0x7f060018;
         public static final int sensorscrollview=0x7f060001;
-        public static final int sessionName=0x7f060011;
-        public static final int setupScroll=0x7f060019;
-        public static final int startStop=0x7f060013;
-        public static final int subsensorlabel=0x7f060018;
-        public static final int text=0x7f06002b;
-        public static final int time=0x7f060012;
-        public static final int toast_layout_root=0x7f060029;
-        public static final int topLayout=0x7f060021;
+        public static final int sessionName=0x7f060012;
+        public static final int setupScroll=0x7f06001a;
+        public static final int startStop=0x7f060014;
+        public static final int subsensorlabel=0x7f060019;
+        public static final int text=0x7f06002c;
+        public static final int time=0x7f060013;
+        public static final int toast_layout_root=0x7f06002a;
+        public static final int topLayout=0x7f060022;
         public static final int toptext=0x7f060006;
         public static final int trobber=0x7f060009;
         public static final int usernameInput=0x7f06000c;
         public static final int usernameLabel=0x7f06000b;
-        public static final int videoButton=0x7f060020;
+        public static final int videoButton=0x7f060021;
     }
     public static final class layout {
         public static final int choosesensor=0x7f030000;
@@ -84,6 +90,7 @@ public final class R {
         public static final int setup=0x7f030007;
         public static final int synctime=0x7f030008;
         public static final int toast_layout=0x7f030009;
+        public static final int toast_layout_x=0x7f03000a;
     }
     public static final class raw {
         public static final int beep=0x7f040000;
@@ -112,6 +119,7 @@ public final class R {
         /**  16 
          */
         public static final int compatible=0x7f050012;
+        public static final int contentDesc=0x7f050036;
         public static final int eula=0x7f05001b;
         public static final int experimentNum=0x7f050027;
         public static final int experiments_no_items=0x7f050019;
@@ -123,7 +131,7 @@ public final class R {
         public static final int heading_rad=0x7f05000d;
         public static final int hello=0x7f050016;
         public static final int iSENSELogo=0x7f050022;
-        public static final int iSENSElogo=0x7f050036;
+        public static final int iSENSElogo=0x7f050037;
         public static final int iSenseConfig=0x7f050018;
         public static final int incompatible=0x7f050013;
         public static final int instructions=0x7f05002f;
