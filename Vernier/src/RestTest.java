df2daf767cb2eb96db055dc1b00272e3bfb2840f
Alan Rosenthal <alan.rosenthal@gmail.com>
2012-07-11 15:12:11 -0400

    started talking to the vernier using the rest api

---
+++ Vernier/src/RestTest.java
@@ -0,0 +1,130 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RestTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+/*		try {
+			StartRecording();
+			Thread.sleep(5000);
+			StopRecording();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		String get_status = GetStatus();
+		try {
+			JSONObject get_status_json = new JSONObject(get_status);
+			int col_size = get_status_json.getJSONObject("views").length();
+			String col_id[] = new String[col_size];//column id
+			String type[] = new String[col_size];//type of date eg time, temp, ph
+			JSONArray col_data[] = new JSONArray[col_size];//column data
+			for (int i = 0;i<col_size;i++){
+				String views = get_status_json.getJSONObject("views").names().get(i).toString();
+				if (get_status_json.getJSONObject("views").getJSONObject(views).has("baseColID")){
+					col_id[i] = get_status_json.getJSONObject("views").getJSONObject(views).getString("baseColID");
+				} else if (get_status_json.getJSONObject("views").getJSONObject(views).has("colID")) {
+					col_id[i] = get_status_json.getJSONObject("views").getJSONObject(views).getString("colID");
+				}
+				type[i] = get_status_json.getJSONObject("columns").getJSONObject(col_id[i]).getString("name");
+				JSONObject get_col_json = new JSONObject(GetColumns(col_id[i]));
+				col_data[i] = get_col_json.getJSONArray("values");
+
+				System.out.println(type[i]);
+				System.out.println(col_data[i]);
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static String StartRecording(){
+		String result = null;
+		try {
+			result = httpGet("http://labquest.local/control/start");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static String StopRecording(){
+		String result = null;
+		try {
+			result = httpGet("http://labquest.local/control/stop");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static String GetInfo(){
+		String result = null;
+		try {
+			result = httpGet("http://labquest.local/info");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static String GetStatus(){
+		String result = null;
+		try {
+			result = httpGet("http://labquest.local/status");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static String GetColumns(String column){
+		String result = null;
+		try {
+			result = httpGet("http://labquest.local/columns/" + column);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static String httpGet(String urlStr) throws IOException {
+		  URL url = new URL(urlStr);
+		  HttpURLConnection conn =
+		      (HttpURLConnection) url.openConnection();
+
+		  if (conn.getResponseCode() != 200) {
+		    throw new IOException(conn.getResponseMessage());
+		  }
+
+		  // Buffer the result into a string
+		  BufferedReader rd = new BufferedReader(
+		      new InputStreamReader(conn.getInputStream()));
+		  StringBuilder sb = new StringBuilder();
+		  String line;
+		  while ((line = rd.readLine()) != null) {
+		    sb.append(line);
+		  }
+		  rd.close();
+		  conn.disconnect();
+		  return sb.toString();
+	  }
+}
