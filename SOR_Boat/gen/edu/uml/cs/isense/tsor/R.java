b7bf508e735fd59f906bfef43a99177e32f838e0
Michael Stowell <StowellMichael@gmail.com>
2012-07-18 10:54:11 -0400

    Made iSENSE Imports an external library with included supplements

--- SOR_Boat/gen/edu/uml/cs/isense/tsor/R.java
+++ SOR_Boat/gen/edu/uml/cs/isense/tsor/R.java
@@ -9,48 +9,63 @@ package edu.uml.cs.isense.tsor;
 
 public final class R {
     public static final class array {
-        public static final int location_array=0x7f050000;
-        public static final int school_array=0x7f050001;
+        public static final int location_array=0x7f060000;
+        public static final int school_array=0x7f060001;
     }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int checkmark=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int red_x=0x7f020005;
+        public static final int toast_background_check=0x7f020006;
+        public static final int toast_background_x=0x7f020007;
     }
     public static final class id {
-        public static final int ImageViewLogo=0x7f060001;
-        public static final int field1Field=0x7f060007;
-        public static final int field1Label=0x7f060006;
-        public static final int field2Field=0x7f060009;
-        public static final int field2Label=0x7f060008;
-        public static final int field3Field=0x7f06000b;
-        public static final int field3Label=0x7f06000a;
-        public static final int field4Field=0x7f06000d;
-        public static final int field4Label=0x7f06000c;
-        public static final int field5Field=0x7f06000f;
-        public static final int field5Label=0x7f06000e;
-        public static final int field6Field=0x7f060011;
-        public static final int field6Label=0x7f060010;
-        public static final int field7Field=0x7f060013;
-        public static final int field7Label=0x7f060012;
-        public static final int field8Field=0x7f060015;
-        public static final int field8Label=0x7f060014;
-        public static final int location=0x7f060005;
-        public static final int locationLabel=0x7f060004;
-        public static final int schoolLabel=0x7f060002;
-        public static final int schools=0x7f060003;
-        public static final int setupScroll=0x7f060000;
-        public static final int submitButton=0x7f060016;
+        public static final int ImageViewLogo=0x7f070001;
+        public static final int field1Field=0x7f070007;
+        public static final int field1Label=0x7f070006;
+        public static final int field2Field=0x7f070009;
+        public static final int field2Label=0x7f070008;
+        public static final int field3Field=0x7f07000b;
+        public static final int field3Label=0x7f07000a;
+        public static final int field4Field=0x7f07000d;
+        public static final int field4Label=0x7f07000c;
+        public static final int field5Field=0x7f07000f;
+        public static final int field5Label=0x7f07000e;
+        public static final int field6Field=0x7f070011;
+        public static final int field6Label=0x7f070010;
+        public static final int field7Field=0x7f070013;
+        public static final int field7Label=0x7f070012;
+        public static final int field8Field=0x7f070015;
+        public static final int field8Label=0x7f070014;
+        public static final int image=0x7f070018;
+        public static final int location=0x7f070005;
+        public static final int locationLabel=0x7f070004;
+        public static final int schoolLabel=0x7f070002;
+        public static final int schools=0x7f070003;
+        public static final int setupScroll=0x7f070000;
+        public static final int submitButton=0x7f070016;
+        public static final int text=0x7f070019;
+        public static final int toast_layout_root=0x7f070017;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int toast_layout=0x7f030001;
+        public static final int toast_layout_x=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int button_text=0x7f040002;
-        public static final int hello=0x7f040000;
-        public static final int location_prompt=0x7f040003;
-        public static final int school_prompt=0x7f040004;
+        public static final int app_name=0x7f040000;
+        public static final int button_text=0x7f040003;
+        public static final int contentDesc=0x7f040001;
+        public static final int hello=0x7f040002;
+        public static final int location_prompt=0x7f040004;
+        public static final int school_prompt=0x7f040005;
+    }
+    public static final class style {
+        public static final int AppTheme=0x7f050000;
     }
 }
