5a7975cbc62dbcc0c8f2938203db5e976b913486
Alan Rosenthal <alan.rosenthal@gmail.com>
2012-07-18 11:54:40 -0400

    restructed file system

--- Vernier/src/RestTest/RestTest.java
+++ Vernier/src/RestTest/RestTest.java
@@ -1,19 +1,16 @@
+package RestTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
-import com.sun.xml.internal.ws.org.objectweb.asm.Type;
-
-import REST.RestAPI;
 import REST.columnMatchException;
 import REST.isenseInterface;
 import REST.loginException;
