2d5274ed9bee7b57b1d2cdd287942e6f70f6d92b
Michael Stowell <StowellMichael@gmail.com>
2012-07-23 13:20:33 -0400

    Added no-gps, force-stop (via home button), and recording-timed-out dialogs as activities

--- Android/Data_Collector/src/edu/uml/cs/isense/collector/ForceStop.java
+++ Android/Data_Collector/src/edu/uml/cs/isense/collector/ForceStop.java
@@ -7,24 +7,30 @@ import android.view.View.OnClickListener;
 import android.view.WindowManager.LayoutParams;
 import android.widget.Button;
 
-public class NoIsense extends Activity {
+public class ForceStop extends Activity {
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.no_isense);
+		setContentView(R.layout.force_stop);
 		
 		getWindow().setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
 		
-		final Button ok = (Button) findViewById(R.id.no_isense_ok);
+		final Button ok = (Button) findViewById(R.id.force_stop_ok);
 		ok.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				finish();
-			}
+			}	
 		});
 		
 	}
 	
+	@Override
+	public void onBackPressed() {
+		setResult(RESULT_CANCELED);
+		finish();
+	}
 }
