49de5fea72a1d3d1fe6a1a77dbcb196b6a149887
Michael Stowell <StowellMichael@gmail.com>
2012-08-06 14:22:51 -0400

    Just about done with splash tabbed-layout.  Is functional.

--- Android/Data_Collector/src/edu/uml/cs/isense/collector/splash/SplashEula.java
+++ Android/Data_Collector/src/edu/uml/cs/isense/collector/splash/SplashEula.java
@@ -2,14 +2,28 @@ package edu.uml.cs.isense.collector.splash;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import edu.uml.cs.isense.collector.R;
 
 public class SplashEula extends Activity {
+
+	public static boolean eulaIsChecked = false;
 	
     public void onCreate(Bundle savedInstanceState) {
     	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_eula);
+        
+        final CheckBox eulaCheck = (CheckBox) findViewById(R.id.eula_check_box);
+        eulaCheck.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				eulaIsChecked = eulaCheck.isChecked();
+			}
+        });
         
     }
     
