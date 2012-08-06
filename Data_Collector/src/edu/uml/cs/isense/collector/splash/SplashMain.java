907a378a3807b71a0abad0589d7d8d2fb5d52d5c
Michael Stowell <StowellMichael@gmail.com>
2012-08-06 12:29:00 -0400

    Added tabs to splash screen. New makeover time.

---
+++ Data_Collector/src/edu/uml/cs/isense/collector/splash/SplashMain.java
@@ -0,0 +1,16 @@
+package edu.uml.cs.isense.collector.splash;
+
+import android.app.Activity;
+import android.os.Bundle;
+import edu.uml.cs.isense.collector.R;
+ 
+public class SplashMain extends Activity {
+	
+    public void onCreate(Bundle savedInstanceState) {
+    	
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_main);
+        
+    }
+    
+}
