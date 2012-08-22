215365882368dfe5509c8e97e6343b8564bacb37
Michael Stowell <StowellMichael@gmail.com>
2012-08-22 09:00:19 -0400

    Finished skeletal layouts for app, still no menu though

---
+++ Android/uPPT_Reader/src/edu/uml/cs/isense/uppt/SdCardFailure.java
@@ -0,0 +1,35 @@
+package edu.uml.cs.isense.uppt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager.LayoutParams;
+import android.widget.Button;
+import edu.uml.cs.isense.uppt.R;
+
+public class SdCardFailure extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sd_card_failure);
+		
+		getWindow().setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		
+		final Button back = (Button) findViewById(R.id.sd_card_failure_back);
+		back.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				finish();
+			}	
+		});
+		
+	}
+	
+	@Override
+	public void onBackPressed() {
+		finish();
+	}
+}
