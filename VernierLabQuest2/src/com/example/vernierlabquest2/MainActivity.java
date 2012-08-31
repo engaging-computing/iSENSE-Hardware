b9bcf9cd770f4b859fbd5bee6787b90242412af3
Alan Rosenthal <alan.rosenthal@gmail.com>
2012-08-31 16:42:58 -0400

    added log cat messages

--- VernierLabQuest2/src/com/example/vernierlabquest2/MainActivity.java
+++ VernierLabQuest2/src/com/example/vernierlabquest2/MainActivity.java
@@ -31,6 +31,7 @@ import android.widget.TextView;
 import edu.uml.cs.isense.comm.RestAPI;
 
 public class MainActivity extends Activity {
+	private String tag = "MainActivity";
 	private RestAPI rapi;
 
 	private TextView iSENSEStatus;
@@ -46,10 +47,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		rapi = RestAPI
-				.getInstance(
-						(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE),
-						this);
+		rapi = RestAPI.getInstance((ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE), this);
 
 		LabQuestStatus = (TextView) findViewById(R.id.labquest_status_text);
 		iSENSEStatus = (TextView) findViewById(R.id.isense_status_text);
@@ -62,6 +60,7 @@ public class MainActivity extends Activity {
 		LabQuestConnect.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				Log.v(tag, "Connect to LabQuest2");
 				LabQuestConnect();
 			}
 		});
@@ -69,8 +68,8 @@ public class MainActivity extends Activity {
 		iSENSEUpload.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				Log.v(tag, "Connect to LabQuest2");
 				iSENSEUpload();
-
 			}
 		});
 	}
@@ -80,38 +79,41 @@ public class MainActivity extends Activity {
 		String iSENSEUser = sp.getString("isense_user", "");
 		String iSENSEPass = sp.getString("isense_pass", "");
 		String iSENSEExpID = sp.getString("isense_expid", "");
-		Long iSENSEDevMode = sp.getLong("isense_dev_mode",0);
-		if (iSENSEDevMode == 1)
-		{
-			rapi.useDev(true);
-			Log.v("Tag","Using iSENSE Dev");
-		}		else
-		{
-			rapi.useDev(false);
-			Log.v("Tag","Using iSENSE");
 
+		// Set iSENSEDev/iSENSE
+		if (sp.getLong("isense_dev_mode", 0) == 1) {
+			rapi.useDev(true);
+			Log.v(tag, "Using iSENSE Dev");
+		} else {
+			rapi.useDev(false);
+			Log.v(tag, "Using iSENSE");
 		}
 
+		// Log in to iSENSE
 		if (rapi.login(iSENSEUser, iSENSEPass)) {
-			Log.v("Tag", "Logged in");
-			iSENSEStatus.setText(getResources().getString(
-					R.string.isense_status_logged_in));
+			Log.v(tag, "Logged in");
+			iSENSEStatus.setText(getResources().getString(R.string.isense_status_logged_in));
 		} else {
-			Log.v("Tag", "Invalid User/Pass");
-			iSENSEStatus.setText(getResources().getString(
-					R.string.isense_status_logged_in_error));
+			Log.v(tag, "Invalid User/Pass");
+			iSENSEStatus.setText(getResources().getString(R.string.isense_status_logged_in_error));
 			return false;
 		}
-		
-		Log.v("TAG",SessionName.getText().toString());
-		int iSENSESessionID = rapi.createSession(iSENSEExpID, SessionName.getText().toString(), "Uploaded with the iSENSE LabQuest2 App!","","","");
+
+		// Create Session
+		int iSENSESessionID = rapi.createSession(iSENSEExpID, SessionName.getText().toString(),
+				"Uploaded with the iSENSE LabQuest2 App!", "", "", "");
+
+		// Get Experiment Fields
+
 		JSONArray temp = new JSONArray();
 		JSONArray row = new JSONArray();
 		row.put("1").put("2").put("3");
 		temp.put(row);
 		temp.put(row);
-		Log.v("A",temp.toString());
-		rapi.putSessionData(iSENSESessionID,iSENSEExpID,temp);
+		Log.v(tag, "JSON:" + temp.toString());
+
+		// Put Data to Session
+		rapi.putSessionData(iSENSESessionID, iSENSEExpID, temp);
 		return true;
 	}
 
@@ -126,6 +128,7 @@ public class MainActivity extends Activity {
 
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu, menu);
+
 		return true;
 	}
 
@@ -134,11 +137,12 @@ public class MainActivity extends Activity {
 		Intent i;
 		switch (item.getItemId()) {
 		case R.id.labquest_settings:
+			Log.v(tag, "Start Activity LabQuestSettings");
 			i = new Intent(this, LabQuestSettings.class);
 			startActivity(i);
 			return true;
 		case R.id.isense_settings:
-			// iSENSEStatus.setText(getResources().getString(R.string.isense_status_logged_in));
+			Log.v(tag, "Start Activity iSENSESettings");
 			i = new Intent(this, iSENSESettings.class);
 			startActivity(i);
 			return true;
@@ -156,8 +160,7 @@ public class MainActivity extends Activity {
 		}
 
 		// Buffer the result into a string
-		BufferedReader rd = new BufferedReader(new InputStreamReader(
-				conn.getInputStream()));
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 		StringBuilder sb = new StringBuilder();
 		String line;
 		while ((line = rd.readLine()) != null) {
@@ -198,10 +201,9 @@ public class MainActivity extends Activity {
 				for (String j : i) {
 					temp = temp + "," + j;
 				}
-				Log.v("Type", temp);
+				Log.v(tag, temp);
 			}
-			LabQuestStatus.setText(getResources().getString(
-					R.string.labquest_status_connected));
+			LabQuestStatus.setText(getResources().getString(R.string.labquest_status_connected));
 			super.onPostExecute(result);
 		}
 
@@ -216,11 +218,11 @@ public class MainActivity extends Activity {
 				JSONObject get_status_json;
 				get_status_json = new JSONObject(GetStatus(LabQuestIP));
 				int col_size = get_status_json.getJSONObject("views").length();
-				long start_time = Long.parseLong(get_status_json
-						.getString("columnListTimeStamp")) * 1000;// time
-																	// started,
-																	// in unix
-																	// milliseconds
+				long start_time = Long.parseLong(get_status_json.getString("columnListTimeStamp")) * 1000;// time
+																											// started
+																											// in
+																											// unix
+																											// ms
 				String col_id[] = new String[col_size];// column id
 				String col_type[] = new String[col_size];// type of date eg
 															// time, temp, ph
@@ -229,21 +231,14 @@ public class MainActivity extends Activity {
 				// strings
 				for (int i = 0; i < col_size; i++) {// loop through all the
 													// columns available
-					String views = get_status_json.getJSONObject("views")
-							.names().getString(i);
-					if (get_status_json.getJSONObject("views")
-							.getJSONObject(views).has("baseColID")) {
-						col_id[i] = get_status_json.getJSONObject("views")
-								.getJSONObject(views).getString("baseColID");
-					} else if (get_status_json.getJSONObject("views")
-							.getJSONObject(views).has("colID")) {
-						col_id[i] = get_status_json.getJSONObject("views")
-								.getJSONObject(views).getString("colID");
+					String views = get_status_json.getJSONObject("views").names().getString(i);
+					if (get_status_json.getJSONObject("views").getJSONObject(views).has("baseColID")) {
+						col_id[i] = get_status_json.getJSONObject("views").getJSONObject(views).getString("baseColID");
+					} else if (get_status_json.getJSONObject("views").getJSONObject(views).has("colID")) {
+						col_id[i] = get_status_json.getJSONObject("views").getJSONObject(views).getString("colID");
 					}
-					col_type[i] = get_status_json.getJSONObject("columns")
-							.getJSONObject(col_id[i]).getString("name");
-					JSONObject get_col_json = new JSONObject(GetColumns(
-							LabQuestIP, col_id[i]));
+					col_type[i] = get_status_json.getJSONObject("columns").getJSONObject(col_id[i]).getString("name");
+					JSONObject get_col_json = new JSONObject(GetColumns(LabQuestIP, col_id[i]));
 					col_data[i] = get_col_json.getString("values");
 				}
 				// TODO change this to JSON Array
@@ -254,20 +249,16 @@ public class MainActivity extends Activity {
 				ArrayList<String> type = new ArrayList<String>();
 				for (int i = 0; i < col_size; i++) {
 					ArrayList<String> temp = new ArrayList<String>();
-					if ((col_type[i].compareTo("Time") == 0)
-							&& (!type.contains("Time"))) { // time
-						StringTokenizer time_tokenized = new StringTokenizer(
-								col_data[i], "[,]");
+					if ((col_type[i].compareTo("Time") == 0) && (!type.contains("Time"))) { // time
+						StringTokenizer time_tokenized = new StringTokenizer(col_data[i], "[,]");
 						while (time_tokenized.hasMoreTokens()) {
-							temp.add(Long.toString((long) Double
-									.parseDouble(time_tokenized.nextToken())
-									* 1000 + start_time));
+							temp.add(Long.toString((long) Double.parseDouble(time_tokenized.nextToken()) * 1000
+									+ start_time));
 						}
 						data.add(temp);
 						type.add(col_type[i]);
 					} else if (col_type[i].compareTo("Time") != 0) {
-						StringTokenizer data_tokenized = new StringTokenizer(
-								col_data[i], "[,]");
+						StringTokenizer data_tokenized = new StringTokenizer(col_data[i], "[,]");
 						while (data_tokenized.hasMoreTokens()) {
 							temp.add(data_tokenized.nextToken());
 						}
