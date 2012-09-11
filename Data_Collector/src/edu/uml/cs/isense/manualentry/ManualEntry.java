9a5e679d229626e1707177eb357b8166668f8fff
Michael Stowell <StowellMichael@gmail.com>
2012-09-11 10:33:09 -0400

    Added menu items, login, experiment

--- Data_Collector/src/edu/uml/cs/isense/manualentry/ManualEntry.java
+++ Data_Collector/src/edu/uml/cs/isense/manualentry/ManualEntry.java
@@ -3,27 +3,47 @@ package edu.uml.cs.isense.manualentry;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import edu.uml.cs.isense.collector.R;
+import edu.uml.cs.isense.collector.splash.Splash;
 import edu.uml.cs.isense.comm.RestAPI;
+import edu.uml.cs.isense.complexdialogs.ExperimentDialog;
+import edu.uml.cs.isense.complexdialogs.LoginActivity;
 import edu.uml.cs.isense.objects.ExperimentField;
+import edu.uml.cs.isense.supplements.ObscuredSharedPreferences;
 import edu.uml.cs.isense.waffle.Waffle;
 
 public class ManualEntry extends Activity implements OnClickListener {
 
-	private static final int REQUEST_EXP_ID = 1;
-
+	private static final int LOGIN_REQUESTED = 100;
+	private static final int EXPERIMENT_REQUESTED = 101;
+	
 	private Waffle w;
 	private RestAPI rapi;
-	
+
 	private Button uploadData;
 	private Button saveData;
 	private Button clearData;
+	
+	private static Context mContext;
+	
+	private TextView loginLabel;
+	private TextView experimentLabel;
+	
+	private SharedPreferences loginPrefs;
+	private SharedPreferences expPrefs;
 
 	private LinearLayout dataFieldEntryList;
 
@@ -31,17 +51,39 @@ public class ManualEntry extends Activity implements OnClickListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.manual_entry);
+		
+		mContext = this;
+		
+		rapi = RestAPI
+				.getInstance(
+						(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE),
+						getApplicationContext());
+		
+		loginPrefs = new ObscuredSharedPreferences(
+				Splash.mContext,
+				Splash.mContext.getSharedPreferences(
+						"USER_INFO", Context.MODE_PRIVATE));
+		
+		expPrefs = getSharedPreferences("EID", 0);
+		
+		loginLabel = (TextView) findViewById(R.id.loginLabel);
+		loginLabel.setText(getResources().getString(R.string.loggedInAs) +
+				loginPrefs.getString("username", ""));
+		
+		experimentLabel = (TextView) findViewById(R.id.experimentLabel);
+		experimentLabel.setText(getResources().getString(R.string.usingExperiment) +
+				expPrefs.getString("experiment_id", ""));
 
 		uploadData = (Button) findViewById(R.id.manual_upload);
 		saveData = (Button) findViewById(R.id.manual_save);
 		clearData = (Button) findViewById(R.id.manual_clear);
-		
+
 		w = new Waffle(this);
-		
+
 		dataFieldEntryList = (LinearLayout) findViewById(R.id.field_view);
 
 		Intent iGetExpId = new Intent(this, ManualEntrySetup.class);
-		startActivityForResult(iGetExpId, REQUEST_EXP_ID);
+		//startActivityForResult(iGetExpId, REQUEST_EXP_ID);
 
 	}
 
@@ -63,35 +105,108 @@ public class ManualEntry extends Activity implements OnClickListener {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		
-		if (requestCode == REQUEST_EXP_ID) {
+
+		if (requestCode == LOGIN_REQUESTED) {
 			if (resultCode == RESULT_OK) {
-				int eid = data.getIntExtra("eid", -1);
+				String returnCode = data.getStringExtra("returnCode");
+
+				if (returnCode.equals("Success")) {
+					
+					String loginName = loginPrefs.getString("username", "");
+					if (loginName.length() >= 18)
+						loginName = loginName.substring(0, 18) + "...";
+					loginLabel.setText(getResources()
+							.getString(R.string.loggedInAs) + loginName);
+					w.make("Login successful", Waffle.LENGTH_LONG,
+							Waffle.IMAGE_CHECK);
+				} else if (returnCode.equals("Failed")) {
+					Intent i = new Intent(mContext, LoginActivity.class);
+					startActivityForResult(i, LOGIN_REQUESTED);
+				} else {
+					// should never get here
+				}
+			}
+		} else if (requestCode == EXPERIMENT_REQUESTED) {
+			if (resultCode == RESULT_OK) {
+				String eidString = data.getStringExtra("eid");
+				int eid = -1;
+				if (!eidString.equals("")) {
+					eid = Integer.parseInt(eidString);
+				}
+				
+				experimentLabel.setText(getResources()
+						.getString(R.string.usingExperiment) +
+						eid);
+				
 				fillDataFieldEntryList(eid);
 				rapi.getExperimentFields(eid);
-			} else {				
-				w.make("Ballz");
 			}
 		}
 	}
 
 	private void fillDataFieldEntryList(int eid) {
 		ArrayList<ExperimentField> fieldOrder = rapi.getExperimentFields(eid);
-		for (ExperimentField expField: fieldOrder) {
-			
+		for (ExperimentField expField : fieldOrder) {
+
 		}
 	}
-	
+
 	private void clearFields() {
-		//TODO
+		// TODO
 	}
-	
+
 	private void saveFields() {
-		//TODO
+		// TODO
+	}
+
+	private void uploadFields() {
+		// TODO
+	}
+
+	// Overridden to prevent user from exiting app unless back button is pressed
+	// twice
+	@Override
+	public void onBackPressed() {
+
+		if (!w.isDisplaying)
+			w.make("Double press \"Back\" to exit.", Waffle.LENGTH_SHORT,
+					Waffle.IMAGE_CHECK);
+		else if (w.canPerformTask)
+			super.onBackPressed();
+
 	}
 	
-	private void uploadFields() {
-		//TODO
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		super.onCreateOptionsMenu(menu);
+		
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_manual, menu);
+
+		return true;
+
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		
+		case R.id.menu_item_manual_experiment:
+			Intent iExperiment = new Intent(mContext, ExperimentDialog.class);
+			startActivityForResult(iExperiment, EXPERIMENT_REQUESTED);
+			
+			return true;
+			
+		case R.id.menu_item_manual_login:
+			Intent iLogin = new Intent(mContext, LoginActivity.class);
+			startActivityForResult(iLogin, LOGIN_REQUESTED);
+			
+			return true;
+			
+		}
+		return false;
+	
 	}
 
 }
