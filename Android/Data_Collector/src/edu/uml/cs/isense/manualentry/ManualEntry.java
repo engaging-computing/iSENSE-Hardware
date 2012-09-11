9d2df1c4aaf65dacf8ace603501801d50bffd8af
Jeremy Poulin <jaypoulz@gmail.com>
2012-09-11 10:39:16 -0400

    finished addDataField and clearFields

--- Android/Data_Collector/src/edu/uml/cs/isense/manualentry/ManualEntry.java
+++ Android/Data_Collector/src/edu/uml/cs/isense/manualentry/ManualEntry.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import edu.uml.cs.isense.collector.R;
 import edu.uml.cs.isense.comm.RestAPI;
 import edu.uml.cs.isense.objects.ExperimentField;
@@ -18,9 +20,14 @@ public class ManualEntry extends Activity implements OnClickListener {
 
 	private static final int REQUEST_EXP_ID = 1;
 
+	private static final int TYPE_DEFAULT = 1;
+	private static final int TYPE_LATITUDE = 2;
+	private static final int TYPE_LONGITUDE = 3;
+	private static final int TYPE_TIME = 4;
+
 	private Waffle w;
 	private RestAPI rapi;
-	
+
 	private Button uploadData;
 	private Button saveData;
 	private Button clearData;
@@ -35,9 +42,9 @@ public class ManualEntry extends Activity implements OnClickListener {
 		uploadData = (Button) findViewById(R.id.manual_upload);
 		saveData = (Button) findViewById(R.id.manual_save);
 		clearData = (Button) findViewById(R.id.manual_clear);
-		
+
 		w = new Waffle(this);
-		
+
 		dataFieldEntryList = (LinearLayout) findViewById(R.id.field_view);
 
 		Intent iGetExpId = new Intent(this, ManualEntrySetup.class);
@@ -63,13 +70,16 @@ public class ManualEntry extends Activity implements OnClickListener {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		
+
 		if (requestCode == REQUEST_EXP_ID) {
 			if (resultCode == RESULT_OK) {
 				int eid = data.getIntExtra("eid", -1);
-				fillDataFieldEntryList(eid);
-				rapi.getExperimentFields(eid);
-			} else {				
+				if (eid != -1) {
+					fillDataFieldEntryList(eid);
+					rapi.getExperimentFields(eid);
+				} else
+					w.make("Ballz x2");
+			} else {
 				w.make("Ballz");
 			}
 		}
@@ -77,21 +87,50 @@ public class ManualEntry extends Activity implements OnClickListener {
 
 	private void fillDataFieldEntryList(int eid) {
 		ArrayList<ExperimentField> fieldOrder = rapi.getExperimentFields(eid);
-		for (ExperimentField expField: fieldOrder) {
-			
+		for (ExperimentField expField : fieldOrder) {
+			if (expField.type_id == expField.GEOSPACIAL) {
+				if (expField.unit_id == expField.UNIT_LATITUDE) {
+					addDataField(expField, TYPE_LATITUDE);
+				} else {
+					addDataField(expField, TYPE_LONGITUDE);
+				}
+			} else if (expField.type_id == expField.TIME) {
+				addDataField(expField, TYPE_TIME);
+			} else {
+				addDataField(expField, TYPE_DEFAULT);
+			}
 		}
 	}
-	
+
+	private void addDataField(ExperimentField expField, int type) {
+		View dataField = View.inflate(this, R.id.manual_dataField, null);
+		TextView fieldName = (TextView) dataField
+				.findViewById(R.id.manual_dataFieldName);
+		fieldName.setText(expField.field_name);
+		if (type != TYPE_DEFAULT) {
+			EditText fieldContents = (EditText) dataField
+					.findViewById(R.id.manual_dataFieldContents);
+			fieldContents.setText("Auto");
+			fieldContents.setEnabled(false);
+		}
+		dataFieldEntryList.addView(dataField);
+	}
+
 	private void clearFields() {
-		//TODO
+		for (int i = dataFieldEntryList.getChildCount(); i > 0; i--) {
+			EditText dataFieldContents = (EditText) dataFieldEntryList
+					.getChildAt(i).findViewById(R.id.manual_dataFieldContents);
+			if (dataFieldContents.isEnabled())
+				dataFieldContents.setText("");
+		}
 	}
-	
+
 	private void saveFields() {
-		//TODO
+		// TODO
 	}
-	
+
 	private void uploadFields() {
-		//TODO
+		// TODO
 	}
 
 }
