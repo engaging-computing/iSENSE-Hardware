33c8fa104dfe8e3b1e9348cf2e12e1a52888422e
Jeremy Poulin <jaypoulz@gmail.com>
2012-10-09 12:14:53 -0400

    Added basic structure for queue saving to replace smart uploading.

---
+++ Android/General_Pictures/src/edu/uml/cs/isense/shared/DeleteDataSetFromUploadQueue.java
@@ -0,0 +1,44 @@
+package edu.uml.cs.isense.shared;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import edu.uml.cs.isense.genpics.R;
+
+public class DeleteDataSetFromUploadQueue extends Activity {
+	
+	private Button no, yes;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.queueblock_delete);
+		super.onCreate(savedInstanceState);
+		
+		no = (Button) findViewById(R.id.queue_delete_no);
+		yes = (Button) findViewById(R.id.queue_delete_yes);
+		
+		no.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				setResult(RESULT_CANCELED);
+				finish();				
+			}
+			
+		});
+		
+		yes.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				finish();
+			}
+			
+		});
+		
+	}
+	
+}
---
+++ Android/General_Pictures/src/edu/uml/cs/isense/shared/QueueParentAssets.java
@@ -0,0 +1,22 @@
+package edu.uml.cs.isense.shared;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import android.content.Context;
+import edu.uml.cs.isense.genpics.DataSet;
+
+public class QueueParentAssets {
+	public Queue<DataSet> uploadQueue;
+	public Queue<DataSet> mirrorQueue;
+	public String parentName;
+	public Context mContext;
+
+	QueueParentAssets(Queue<DataSet> queue, String parentName, Context context) {
+		this.uploadQueue = queue;
+		this.parentName = parentName;
+		this.mContext = context;
+		this.mirrorQueue = new LinkedList<DataSet>();
+		mirrorQueue.addAll(queue);
+	}
+}
