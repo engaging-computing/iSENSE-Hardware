4f9e76fe62db30f0b8154b0ca137a241225a0a2b
Jeremy Poulin <jaypoulz@gmail.com>
2012-10-19 07:49:58 -0400

    Fixed lint issues

---
+++ Android/Group_Collector/src/edu/uml/cs/isense/gcollector/Setup.java
@@ -0,0 +1,16 @@
+package edu.uml.cs.isense.gcollector;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Setup extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.setup);
+		super.onCreate(savedInstanceState);
+	}
+	
+	
+
+}
