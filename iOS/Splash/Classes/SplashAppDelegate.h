fc55ba1972bd46d75b9ded73e0f9e03e5f29cae1
Michael Stowell <StowellMichael@gmail.com>
2012-12-05 12:47:31 -0500

    Working skeleton for the splash of the data collector app.  Orb rotates awkwardly

--- iOS/Splash/Classes/SplashAppDelegate.h
+++ iOS/Splash/Classes/SplashAppDelegate.h
@@ -2,21 +2,36 @@
 //  SplashAppDelegate.h
 //  Splash
 //
-//  Created by CS Admin on 11/21/12.
+//  Created by CS Admin on 12/4/12.
 //  Copyright 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@class SplashViewController;
-
 @interface SplashAppDelegate : NSObject <UIApplicationDelegate> {
-    UIWindow *window;
-    SplashViewController *viewController;
+    IBOutlet UIWindow *window;
+	IBOutlet UITabBarController *tbc;
+	IBOutlet UIImageView *orb;
+	IBOutlet UITextView *aboutText;
+	IBOutlet UITextView *guideText;
+	IBOutlet UIButton *automatic;
+	IBOutlet UIButton *manual;
+	
 }
 
+-(void)rotateImage:(UIImageView *)image duration:(NSTimeInterval)duration 
+			 curve:(int)curve degrees:(CGFloat)degrees;
+
+-(NSString *) getString:(NSString *)label;
+
+
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-@property (nonatomic, retain) IBOutlet SplashViewController *viewController;
+@property (nonatomic, retain) IBOutlet UITabBarController *tbc;
+@property (nonatomic, retain) IBOutlet UIImageView *orb;
+@property (nonatomic, retain) IBOutlet UITextView *aboutText;
+@property (nonatomic, retain) IBOutlet UITextView *guideText;
+@property (nonatomic, retain) IBOutlet UIButton *automatic;
+@property (nonatomic, retain) IBOutlet UIButton *manual;
 
 @end
 
