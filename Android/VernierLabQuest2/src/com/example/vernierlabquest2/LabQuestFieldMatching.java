e3c02cfe52796c43a28fad926a4f75f74e68c139
Alan Rosenthal <alan.rosenthal@gmail.com>
2013-01-03 13:03:20 -0500

    added base for manual field matching

---
+++ Android/VernierLabQuest2/src/com/example/vernierlabquest2/LabQuestFieldMatching.java
@@ -0,0 +1,15 @@
+package com.example.vernierlabquest2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LabQuestFieldMatching extends Activity {
+	private String tag = "LabQuestFieldMatching";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.labquest_field_matching);
+		
+	}
+
+}
