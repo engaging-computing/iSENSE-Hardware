9e6c91428d66dcae63b0fcd9418498ad9e688962
stowellm <stowellm@ECG-iMAC.local>
2013-01-24 12:49:23 -0500

    Figured out a workaround to the Experiment Selection menu. Defined helpful macros

--- iOS/Data_Collector/Classes/ManualViewController.m
+++ iOS/Data_Collector/Classes/ManualViewController.m
@@ -14,6 +14,11 @@
 #define MENU_EXPERIMENT 1
 #define MENU_LOGIN 2
 
+#define OPTION_CANCELED 0
+#define OPTION_ENTER_EXPERIMENT_NUMBER 1
+#define OPTION_BROWSE_EXPERIMENTS 2
+#define OPTION_SCAN_QR_CODE 3
+
 @implementation ManualViewController
 
 @synthesize logo, loggedInAsLabel, expNumLabel, save, clear, sessionNameInput, media, scrollView;
@@ -131,13 +136,13 @@
             
 		case MENU_EXPERIMENT:
             message = [[UIAlertView alloc] initWithTitle:@"Experiment Selection"
-                                                 message:@"Enter an experiment #, browse experiments, or scan a QR code"
+                                                 message:nil
                                                 delegate:self
                                        cancelButtonTitle:@"Cancel"
-                                       otherButtonTitles:@"Okay", @"Browse", @"Scan QR", nil];
+                                       otherButtonTitles:@"Enter Experiment #", @"Browse", @"Scan QR Code", nil];
             
             message.tag = MENU_EXPERIMENT;
-            [message setAlertViewStyle:UIAlertViewStylePlainTextInput];
+            //[message setAlertViewStyle:UIAlertViewStylePlainTextInput];
             
 			break;
             
@@ -166,21 +171,21 @@
 
 - (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
     if (actionSheet.tag == MENU_LOGIN) {
-        if (buttonIndex != 0) {
+        
+        if (buttonIndex != OPTION_CANCELED) {
             NSString *usernameInput = [[actionSheet textFieldAtIndex:0] text];
             NSString *passwordInput = [[actionSheet textFieldAtIndex:1] text];
             [self login:usernameInput withPassword:passwordInput];
         }
         
     } else if (actionSheet.tag == MENU_EXPERIMENT){
-        if (buttonIndex == 0) {
-            NSLog(@"0");
-        } else if (buttonIndex == 1) {
-            NSLog(@"1");
-        } else if (buttonIndex == 2) {
-            NSLog(@"2");
-        } else if (buttonIndex == 3) {
-            NSLog(@"3");
+        
+        if (buttonIndex == OPTION_ENTER_EXPERIMENT_NUMBER) {
+            
+        } else if (buttonIndex == OPTION_BROWSE_EXPERIMENTS) {
+            
+        } else if (buttonIndex == OPTION_SCAN_QR_CODE) {
+            
         }
         
     } else if (actionSheet.tag == MENU_UPLOAD) {
