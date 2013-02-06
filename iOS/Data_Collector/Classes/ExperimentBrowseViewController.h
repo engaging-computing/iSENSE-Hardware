e967e5ea66b4b0da39e81c63b1bf44bae49b4df2
jaypoulz <jaypoulz@gmail.com>
2013-02-06 08:35:45 -0500

    Search is appending properly

--- iOS/Data_Collector/Classes/ExperimentBrowseViewController.h
+++ iOS/Data_Collector/Classes/ExperimentBrowseViewController.h
@@ -8,14 +8,20 @@
 
 #import <UIKit/UIKit.h>
 #import "ExperimentBlock.h"
+#import "ISenseSearch.h"
 
 @interface ExperimentBrowseViewController : UIViewController <UISearchBarDelegate> {
     iSENSE *isenseAPI;
     UIScrollView *scrollView;
     UIActivityIndicatorView *spinner;
+    NSThread *loadExperimentThread;
 }
 
 - (IBAction)onExperimentButtonClicked:(id)caller;
+- (void) updateScrollView:(ISenseSearch *)iSS;
+
+@property (nonatomic, assign) int currentPage;
 
 
 @end
+
--- iOS/Data_Collector/Classes/ExperimentBrowseViewController.m
+++ iOS/Data_Collector/Classes/ExperimentBrowseViewController.m
@@ -8,14 +8,11 @@
 
 #import "ExperimentBrowseViewController.h"
 
-@interface ExperimentBrowseViewController ()
-
-@end
-
 @implementation ExperimentBrowseViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+@synthesize currentPage;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -35,27 +32,37 @@
         self.view.backgroundColor = [UIColor blackColor];
     }
     
+    // Prepare search bar
     UISearchBar *searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 40)];
     searchBar.delegate = self;
     [self.view addSubview:searchBar];
     
-    scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 50, 320, self.view.bounds.size.height)];
+    // Prepare spinner for loading at bottom
+    UIView *bottomSpinnerBlock = [[UIView alloc] initWithFrame:CGRectMake(0, self.view.bounds.size.height - 150, self.view.bounds.size.width, 150)];
+    [self setCenter:bottomSpinnerBlock];
+    [bottomSpinnerBlock addSubview:spinner];
+    [self.view addSubview:bottomSpinnerBlock];
+    
+    // Prepare scrollview
+    scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 50, 320, self.view.bounds.size.height - 150)];
     scrollView.delaysContentTouches = NO;
     [self.view addSubview:scrollView];
-                                                             
     
+    // Prepare rapi
     isenseAPI = [iSENSE getInstance];
-
+    
+    // Load the first 10 experiments.
+   loadExperimentThread = [[NSThread alloc] initWithTarget:self selector:@selector(updateScrollView:) object:[[ISenseSearch alloc] init]];
+    [loadExperimentThread start];
+        
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
@@ -67,29 +74,20 @@
 }
 
 - (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
-    [self handleSearch:searchBar];
+    //[self handleSearch:searchBar];
 }
 
 - (void)handleSearch:(UISearchBar *)searchBar {
     NSLog(@"User searched for %@", searchBar.text);
-    [[scrollView subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
-    NSMutableArray *experiments = [isenseAPI getExperiments:[NSNumber numberWithInt:1] withLimit:[NSNumber numberWithInt:25] withQuery:searchBar.text andSort:@"Recent"];
-    int i = 0, maxHeight = 0;
-    for(Experiment *exp in experiments) {
-        
-        ExperimentBlock *block = [[ExperimentBlock alloc] initWithFrame:CGRectMake(0, i*60, 310, 50)
-                                                        experimentName:exp.name
-                                                        experimentNumber:[exp.experiment_id integerValue]
-                                                        target:self
-                                                        action:@selector(onExperimentButtonClicked:)];
-        [scrollView addSubview:block];
-        i++;
-        maxHeight += 60;
-    }
     
-    CGSize scrollableSize = CGSizeMake(320, maxHeight);
-    [scrollView setContentSize:scrollableSize];
+    if ([loadExperimentThread isExecuting])
+        [loadExperimentThread cancel];
+    
+    loadExperimentThread = [[NSThread alloc] initWithTarget:self selector:@selector(updateScrollView:) object:[[ISenseSearch alloc] initWithQuery:searchBar.text searchType:RECENT page:1 andBuildType:APPEND]];
+    
+    [loadExperimentThread start];
 
+        
     [searchBar resignFirstResponder]; // if you want the keyboard to go away
 }
 
@@ -100,6 +98,58 @@
 
 - (IBAction)onExperimentButtonClicked:(id)caller {
     [caller switchToDarkImage:false];
+}
+
+// Sets our spinner to the middle of the bottom block.
+- (void)setCenter:(UIView *)view {
+    spinner = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    [view addSubview:spinner];
+ //   [spinner startAnimating];
+    CGSize boundsSize = view.bounds.size;
+    CGRect frameToCenter = spinner.frame;
+    // center horizontally
+    if (frameToCenter.size.width < boundsSize.width)
+        frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2;
+    else
+        frameToCenter.origin.x = 0;
+    
+    // center vertically
+    if (frameToCenter.size.height < boundsSize.height)
+        frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2;
+    else
+        frameToCenter.origin.y = 0;
+    
+    spinner.frame = frameToCenter;
+    NSLog(@"The spinner is currently at %f, %f.", spinner.center.x, spinner.center.y);
+}
+
+// Update scrollView by appending or making a new search on a separate thread.
+- (void) updateScrollView:(ISenseSearch *)iSS {
+    [spinner startAnimating];
+    
+    if (iSS.buildType == NEW)
+        [[scrollView subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    
+    NSMutableArray *experiments = [isenseAPI getExperiments:[NSNumber numberWithInt:iSS.page] withLimit:[NSNumber numberWithInt:10] withQuery:iSS.query andSort:[iSS searchTypeToString]];
+    int maxHeight = 0;
+    
+    if (iSS.buildType == APPEND)
+        maxHeight = scrollView.contentSize.height;
+    
+    for(Experiment *exp in experiments) {
+        ExperimentBlock *block = [[ExperimentBlock alloc] initWithFrame:CGRectMake(0, maxHeight, 310, 50)
+                                                         experimentName:exp.name
+                                                       experimentNumber:[exp.experiment_id integerValue]
+                                                                 target:self
+                                                                 action:@selector(onExperimentButtonClicked:)];
+        [scrollView addSubview:block];
+         maxHeight += 60;
+    }
+    
+    CGSize scrollableSize = CGSizeMake(320, maxHeight);
+    [scrollView setContentSize:scrollableSize];
+
+    [spinner stopAnimating];
 }
 
 @end
