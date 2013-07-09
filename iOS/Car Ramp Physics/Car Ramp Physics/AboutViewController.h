77db50fe8bb4ad7339f60eb129fca167a5b1f55b
Virinchi Balabhadrapatruni <Virinchi_Balabhadrapatruni@student.uml.edu>
2013-07-09 14:50:56 -0400

    Added About view controller Added menu and actionsheet for settings

---
+++ iOS/Car Ramp Physics/Car Ramp Physics/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.h
+//  Car Ramp Physics
+//
+//  Created by Virinchi Balabhadrapatruni on 7/9/13.
+//  Copyright (c) 2013 ECG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+
+@property(nonatomic) IBOutlet UITextView *text;
+
+@end
