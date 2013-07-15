70dfda9823506221aca01cbd01548462b3bd9666
stowellm <stowellmichael@gmail.com>
2013-07-15 12:27:07 -0400

    Added Waffles to iSENSE Imports

---
+++ iOS/iSENSE_API/iSENSE_API/WaffleConstants.h
@@ -0,0 +1,22 @@
+//
+//  WaffleConstants.h
+//  iSENSE_API
+//
+//  Created by Michael Stowell on 7/15/13.
+//  Copyright (c) 2013 Jeremy Poulin. All rights reserved.
+//
+
+#ifndef iSENSE_API_WaffleConstants_h
+#define iSENSE_API_WaffleConstants_h
+
+// waffle constants
+#define WAFFLE_LENGTH_SHORT  2.0
+#define WAFFLE_LENGTH_LONG   3.5
+#define WAFFLE_BOTTOM @"bottom"
+#define WAFFLE_TOP @"top"
+#define WAFFLE_CENTER @"center"
+#define WAFFLE_CHECKMARK @"waffle_check"
+#define WAFFLE_RED_X @"waffle_x"
+#define WAFFLE_WARNING @"waffle_warn"
+
+#endif
