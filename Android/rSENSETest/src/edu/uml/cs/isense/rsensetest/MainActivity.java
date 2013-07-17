ba8988970efb96ec26446d3d99c467aedc8734e7
Nicholas Ver Voort <moldorma@gmail.com>
2013-07-17 10:12:24 -0400

    requesttask now returns a proper JSON object from rsense

--- Android/rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
+++ Android/rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			try {
 				URL url = new URL(uri[0]);
 				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-				urlConnection.setRequestProperty("Content-Type", "application/json");
+				urlConnection.setRequestProperty("Accept", "application/json");
 				InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 				try {
 					ByteArrayOutputStream bo = new ByteArrayOutputStream();
