6cf45537a3e28f3e2333e05ee09b7cc7eb7b8e0a
Nicholas Ver Voort <moldorma@gmail.com>
2013-07-17 11:34:44 -0400

    authenticated funtions working

--- Android/rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
+++ Android/rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
@@ -4,8 +4,13 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.CookieHandler;
+import java.net.CookieManager;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import java.net.CookiePolicy;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.AsyncTask;
@@ -21,6 +26,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	Button login, getusers, getprojects;
 	TextView status;
 	String baseURL = "http://129.63.17.17:3000";
+	String authToken = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,8 @@ public class MainActivity extends Activity implements OnClickListener {
 		login.setOnClickListener(this);
 		getusers.setOnClickListener(this);
 		getprojects.setOnClickListener(this);
+		
+		CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
 	}
 
 	@Override
@@ -47,10 +55,13 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		if ( v == login ) {
 			status.setText("clicked login");
+			new RequestTask().execute(baseURL, "login", "username_or_email=testguy&password=1", "POST");
 		} else if ( v == getusers ) {
 			status.setText("clicked get users");
+			new RequestTask().execute(baseURL, "users", "", "GET");
 		} else if ( v == getprojects ) {
-			new RequestTask().execute(baseURL+"/projects");
+			status.setText("clicked get projects");
+			new RequestTask().execute(baseURL, "projects", "authenticity_token="+authToken, "GET");
 		}
 	}
 
@@ -58,12 +69,19 @@ public class MainActivity extends Activity implements OnClickListener {
 	//http://stackoverflow.com/questions/3505930/make-an-http-request-with-android
 	class RequestTask extends AsyncTask<String, String, String>{
 
+		String requestPathBase = "";
+
 		@Override
 		protected String doInBackground(String... uri) {
+			int mstat = 0;
+			requestPathBase = uri[1];
+			System.out.println(authToken);
 			try {
-				URL url = new URL(uri[0]);
+				URL url = new URL(uri[0]+"/"+uri[1]+"?"+uri[2]);
 				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+				urlConnection.setRequestMethod(uri[3]);
 				urlConnection.setRequestProperty("Accept", "application/json");
+				mstat = urlConnection.getResponseCode();
 				InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 				try {
 					ByteArrayOutputStream bo = new ByteArrayOutputStream();
@@ -82,13 +100,21 @@ public class MainActivity extends Activity implements OnClickListener {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			return "Error Reading Data!";
+			return "Status: "+mstat;
 		}
 
 		@Override
 		protected void onPostExecute(String result) {
 			super.onPostExecute(result);
 			System.out.println(result);
+			try {
+				if(requestPathBase.equals("login")) {
+					JSONObject j = new JSONObject(result);
+					authToken = j.getString("authenticity_token");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
