eb82abae22eb4d61b3340ad26ebfd618316b1ee4
Nicholas Ver Voort <moldorma@gmail.com>
2013-07-17 09:05:39 -0400

    hooked up buttons
    Former-commit-id: c4af89057521bb88146af7301f3307e8a1519071

--- rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
+++ rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
@@ -1,22 +1,44 @@
 package edu.uml.cs.isense.rsensetest;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	Button login, getusers;
+	TextView status;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		login = (Button) findViewById(R.id.btn_login);
+		getusers = (Button) findViewById(R.id.btn_getusers);
+		status = (TextView) findViewById(R.id.txt_results);
+		
+		login.setOnClickListener(this);
+		getusers.setOnClickListener(this);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if ( v == login ) {
+			status.setText("clicked login");
+		} else if ( v == getusers ) {
+			status.setText("clicked get users");
+		}
 	}
 
 }
