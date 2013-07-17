810c7a31affec75cb98566c5257fd6d56a4abcc4
Nicholas Ver Voort <moldorma@gmail.com>
2013-07-17 09:55:54 -0400

    can make HTTP requests, but they all return HTML
    Former-commit-id: 4e15f7f1feb96f96b53da36fb53542e59ecb86e0

--- rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
+++ rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
@@ -1,6 +1,14 @@
 package edu.uml.cs.isense.rsensetest;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -10,20 +18,23 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	Button login, getusers;
+	Button login, getusers, getprojects;
 	TextView status;
-	
+	String baseURL = "http://129.63.17.17:3000";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		login = (Button) findViewById(R.id.btn_login);
 		getusers = (Button) findViewById(R.id.btn_getusers);
+		getprojects = (Button) findViewById(R.id.btn_getprojects);
 		status = (TextView) findViewById(R.id.txt_results);
-		
+
 		login.setOnClickListener(this);
 		getusers.setOnClickListener(this);
+		getprojects.setOnClickListener(this);
 	}
 
 	@Override
@@ -38,6 +49,46 @@ public class MainActivity extends Activity implements OnClickListener {
 			status.setText("clicked login");
 		} else if ( v == getusers ) {
 			status.setText("clicked get users");
+		} else if ( v == getprojects ) {
+			new RequestTask().execute(baseURL+"/projects");
+		}
+	}
+
+	//nabbed from StackOverflow
+	//http://stackoverflow.com/questions/3505930/make-an-http-request-with-android
+	class RequestTask extends AsyncTask<String, String, String>{
+
+		@Override
+		protected String doInBackground(String... uri) {
+			try {
+				URL url = new URL(uri[0]);
+				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+				urlConnection.setRequestProperty("Content-Type", "application/json");
+				InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+				try {
+					ByteArrayOutputStream bo = new ByteArrayOutputStream();
+					int i = in.read();
+					while(i != -1) {
+						bo.write(i);
+						i = in.read();
+					}
+					return bo.toString();
+				} catch (IOException e) {
+					return "";
+				}
+				finally {
+					in.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return "Error Reading Data!";
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			System.out.println(result);
 		}
 	}
 
