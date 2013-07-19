6c533a09bfbb96192ff70795356a3387a81d30d3
Nicholas Ver Voort <moldorma@gmail.com>
2013-07-19 09:08:22 -0400

    removed async taskiness from rsense api class
    Former-commit-id: 42e13144badc96ae1288d528ceb8e9cbc97fd557

--- rSENSETest/src/edu/uml/cs/isense/rsensetest/API.java
+++ rSENSETest/src/edu/uml/cs/isense/rsensetest/API.java
@@ -10,82 +10,60 @@ import java.net.CookiePolicy;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.json.JSONObject;
-
-import android.os.AsyncTask;
-
 public class API {
 	String baseURL = "http://129.63.17.17:3000";
 	String authToken = "";
-	
+
 	public API() {
 		CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
 	}
-	
+
 	/*Call this function to log in to iSENSE*/
 	/*Once you've done this you'll be able to call authenticated functions and get data back*/
 	/*Returns true if login succeeds*/
 	public boolean createSession(String username, String password) {
-		new RequestTask().execute(baseURL, "login", "username_or_email="+username+"&password="+password, "POST");
+		makeRequest(baseURL, "login", "username_or_email="+username+"&password="+password, "POST");
 		return true;
 	}
-	
+
 	public void getProjects() {
-		new RequestTask().execute(baseURL, "projects", "authenticity_token="+authToken, "GET");
+		makeRequest(baseURL, "projects", "authenticity_token="+authToken, "GET");
 	}
-	
+
 	/*Authenticated function*/
 	/*Must have called createSession before calling this function*/
 	public void getUsers() {
-		new RequestTask().execute(baseURL, "users", "", "GET");
+		makeRequest(baseURL, "users", "", "GET");
 	}
-	
-	class RequestTask extends AsyncTask<String, String, String>{
 
-		String requestPathBase = "";
+	public String makeRequest(String baseURL, String path, String parameters, String reqType) {
 
-		@Override
-		protected String doInBackground(String... uri) {
-			int mstat = 0;
-			requestPathBase = uri[1];
+		int mstat = 0;
+		try {
+			URL url = new URL(baseURL+"/"+path+"?"+parameters);
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setRequestMethod(reqType);
+			urlConnection.setRequestProperty("Accept", "application/json");
+			mstat = urlConnection.getResponseCode();
+			InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 			try {
-				URL url = new URL(uri[0]+"/"+uri[1]+"?"+uri[2]);
-				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-				urlConnection.setRequestMethod(uri[3]);
-				urlConnection.setRequestProperty("Accept", "application/json");
-				mstat = urlConnection.getResponseCode();
-				InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-				try {
-					ByteArrayOutputStream bo = new ByteArrayOutputStream();
-					int i = in.read();
-					while(i != -1) {
-						bo.write(i);
-						i = in.read();
-					}
-					return bo.toString();
-				} catch (IOException e) {
-					return "";
+				ByteArrayOutputStream bo = new ByteArrayOutputStream();
+				int i = in.read();
+				while(i != -1) {
+					bo.write(i);
+					i = in.read();
 				}
-				finally {
-					in.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+				return bo.toString();
+			} catch (IOException e) {
+				return "";
 			}
-			return "Status: "+mstat;
+			finally {
+				in.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		return "Status: "+mstat;
 
-		@Override
-		protected void onPostExecute(String result) {
-			super.onPostExecute(result);
-			try {
-				if(requestPathBase.equals("login")) {
-					JSONObject j = new JSONObject(result);
-					authToken = j.getString("authenticity_token");
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
 	}
 }
