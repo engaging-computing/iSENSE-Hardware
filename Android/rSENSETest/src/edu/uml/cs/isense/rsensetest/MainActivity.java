5b8a452bb7859440d79bb7ee1771686e592187e6
Nicholas Ver Voort <moldorma@gmail.com>
2013-07-26 11:38:32 -0400

    added pagination parameters to getUsers function

--- Android/rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
+++ Android/rSENSETest/src/edu/uml/cs/isense/rsensetest/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		@Override
 		protected ArrayList<RPerson> doInBackground(Void... params) {
 			if(userName.getText().toString().equals("")) {
-				return api.getUsers();
+				return api.getUsers(1, 10);
 			} else {
 				ArrayList<RPerson> rp = new ArrayList<RPerson>();
 				rp.add(api.getUser(userName.getText().toString()));
