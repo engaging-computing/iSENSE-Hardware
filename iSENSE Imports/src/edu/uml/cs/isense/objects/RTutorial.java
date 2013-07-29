525d7569abbfe4f5dfa93ab4ac91b12b18ddab88
Nicholas Ver Voort <moldorma@gmail.com>
2013-07-29 10:39:12 -0400

    added get tutorial functions
    Former-commit-id: 9a82428c6be78e1ccc2ac33c7cedbac8b34e41ea

---
+++ iSENSE Imports/src/edu/uml/cs/isense/objects/RTutorial.java
@@ -0,0 +1,17 @@
+package edu.uml.cs.isense.objects;
+
+/**
+ * Class that includes information about a particular tutorial on iSENSE.
+ * 
+ * @author iSENSE Android Development Team
+ */
+public class RTutorial {
+    public int tutorial_id;
+    public int like_count;
+    public boolean hidden;
+    public String name = "";
+    public String url = "";
+    public String timecreated = "";
+    public String owner_name = "";
+    public String owner_url = "";
+}
