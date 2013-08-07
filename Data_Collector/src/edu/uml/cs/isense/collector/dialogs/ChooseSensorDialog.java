57f8151f50485bec683416b1adbce1e7dd5a78fe
jaypoulz <jaypoulz@gmail.com>
2013-08-07 10:55:37 -0400

    Trying to stabilize CarRampPhysics
    Former-commit-id: be2484aaad1a7e502ac3c22fe31f7d4225bd3b24

--- Data_Collector/src/edu/uml/cs/isense/collector/dialogs/ChooseSensorDialog.java
+++ Data_Collector/src/edu/uml/cs/isense/collector/dialogs/ChooseSensorDialog.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
