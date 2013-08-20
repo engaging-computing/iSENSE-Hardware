23d94eb02bce5f72a82537e84ea1a1c2fe7fcc8e
stowellm <stowellmichael@gmail.com>
2013-08-20 15:02:22 -0400

    Added documentation for the remainder of the objects in the edu.uml.cs.isense.dfm package

--- Android/iSENSE Imports/src/edu/uml/cs/isense/dfm/SensorCompatibility.java
+++ Android/iSENSE Imports/src/edu/uml/cs/isense/dfm/SensorCompatibility.java
@@ -1,50 +1,73 @@
 package edu.uml.cs.isense.dfm;
 
+/**
+ * This class is used to determine which sensors are typically/potentially
+ * available on the user's device.
+ * 
+ * @author iSENSE Android Development Team
+ *
+ */
 public class SensorCompatibility {
 
+	/**
+	 * Enumeration type that contains the 6 different register-able
+	 * hardware sensor managers.
+	 * 
+	 */
 	public enum SensorTypes {
 		ACCELEROMETER(0), MAGNETIC_FIELD(1), AMBIENT_TEMPERATURE(2), PRESSURE(3), LIGHT(
 				4), ORIENTATION(5);
 
 		private int sensorType;
 
-		SensorTypes(int sensorType) {
+		private SensorTypes(int sensorType) {
 			this.setSensorType(sensorType);
 		}
 
-		public int getSensorType() {
+		private int getSensorType() {
 			return sensorType;
 		}
 
-		public void setSensorType(int sensorType) {
+		private void setSensorType(int sensorType) {
 			this.sensorType = sensorType;
 		}
 
 	}
 
     // Row for each of the sensors
-	public boolean[] compatible = { false, false, false, false, false, false };
+	protected boolean[] compatible = { false, false, false, false, false, false };
 
-	public int[][] compatDispatch = {
+	protected int[][] compatDispatch = {
 			// 0 = false, 1 = true
 			{ 1, 1, 0, 0, 1, 1 }, // Row for api level 8
 			{ 1, 1, 0, 1, 1, 1 }, // Row for api level 9 - 13
 			{ 1, 1, 1, 1, 1, 1 }, // Row for api level 14 +
-	/*
-	 * Columns: 0 - Accelerometer 
-	 *          1 - Magnetic Field
-	 *          2 - Ambient Temperature 
-	 *          3 - Pressure
-	 *          4 - Light 
-	 *          5 - Orientation
-	 */
+	
+//	  Columns: 0 - Accelerometer 
+//	           1 - Magnetic Field
+//	           2 - Ambient Temperature 
+//	           3 - Pressure
+//	           4 - Light 
+//	           5 - Orientation
+//	 
 			
 	};
 
+	/**
+	 * Default constructor for the SensorCompatibility object.
+	 */
 	public SensorCompatibility() {
-
 	}
 
+	/**
+	 * Determines if the passed in sensor type is compatible with the user's device.
+	 * 
+	 * @param sensorType
+	 * 		- The type of hardware sensor you would like to check for compatibility.
+	 * @return
+	 * 		@true  If the sensor type is compatible with the user's device.
+	 *      @false Otherwise
+	 */
 	public boolean isCompatible(SensorTypes sensorType) {
 		return compatible[(sensorType.getSensorType())];
 	}
