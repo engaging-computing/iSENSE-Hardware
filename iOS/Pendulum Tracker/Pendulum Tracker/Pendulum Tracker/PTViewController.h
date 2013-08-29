3af12c84ce6a47fe3f4c462c274fed5192ed727c
Virinchi Balabhadrapatruni <Virinchi_Balabhadrapatruni@student.uml.edu>
2013-08-29 09:45:18 -0400

    Made changes to CRPv2

---
+++ iOS/Pendulum Tracker/Pendulum Tracker/Pendulum Tracker/PTViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTViewController.h
+//  Pendulum Tracker
+//
+//  Created by Virinchi Balabhadrapatruni on 8/28/13.
+//  Copyright (c) 2013 ECG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTViewController : UIViewController
+
+@end
---
+++ iOS/Pendulum Tracker/Pendulum Tracker/Pendulum Tracker/PTViewController.m
@@ -0,0 +1,29 @@
+//
+//  PTViewController.m
+//  Pendulum Tracker
+//
+//  Created by Virinchi Balabhadrapatruni on 8/28/13.
+//  Copyright (c) 2013 ECG. All rights reserved.
+//
+
+#import "PTViewController.h"
+
+@interface PTViewController ()
+
+@end
+
+@implementation PTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
