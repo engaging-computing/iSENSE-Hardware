ace07aeb32779c60baa231da3ac988bfb100b47a
stowellm <stowellmichael@gmail.com>
2013-09-17 13:33:48 -0400

    Changed Data Collector to Uploader, began work on that, changed Uploader and Pendulum Trackers icon

---
+++ Android/Data_Collector/src/edu/uml/cs/isense/collector/splash/Welcome.java
@@ -0,0 +1,120 @@
+package edu.uml.cs.isense.collector.splash;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import edu.uml.cs.isense.collector.R;
+import edu.uml.cs.isense.comm.API;
+import edu.uml.cs.isense.proj.Setup;
+import edu.uml.cs.isense.waffle.Waffle;
+
+public class Welcome extends Activity {
+
+	public static Context mContext;
+	public static Waffle w;
+	private SharedPreferences mPrefs;
+	public API api;
+	
+	private static final int PROJECT_SELECTION_REQUESTED = 100;
+
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome);
+
+		mContext = this;
+		w = new Waffle(mContext);
+		api = API.getInstance(mContext);
+		mPrefs = getSharedPreferences("PROJID_WELCOME", 0);
+		
+		// Action bar customization for API >= 14
+		if (android.os.Build.VERSION.SDK_INT >= 14) {
+			ActionBar bar = getActionBar();
+			bar.setBackgroundDrawable(new ColorDrawable(Color
+					.parseColor("#111133")));
+			bar.setIcon(getResources()
+					.getDrawable(R.drawable.rsense_logo_right));
+			bar.setDisplayShowTitleEnabled(false);
+			int actionBarTitleId = Resources.getSystem().getIdentifier(
+					"action_bar_title", "id", "android");
+			if (actionBarTitleId > 0) {
+				TextView title = (TextView) findViewById(actionBarTitleId);
+				if (title != null) {
+					title.setTextColor(Color.WHITE);
+					title.setTextSize(24.0f);
+				}
+			}
+		}
+
+		// Set listeners for the buttons
+		final Button selectProject = (Button) findViewById(R.id.welcome_select_project);
+		selectProject.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (!api.hasConnectivity())
+					w.make("You need to have internet connectivity to do this",
+							Waffle.LENGTH_LONG, Waffle.IMAGE_WARN);
+				else {
+					Intent iSetup = new Intent(mContext, Setup.class);
+					iSetup.putExtra("from_where", "welcome");
+					startActivityForResult(iSetup, PROJECT_SELECTION_REQUESTED);
+				}
+			}
+		});
+
+		final Button createProject = (Button) findViewById(R.id.welcome_create_project);
+		createProject.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+
+			}
+		});
+		
+		final Button noProject = (Button) findViewById(R.id.welcome_no_project);
+		noProject.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent iSelectMode = new Intent(mContext, SelectMode.class);
+				iSelectMode.putExtra(SelectMode.ENABLE_MANUAL_ENTRY, false);
+				startActivity(iSelectMode);
+			}
+		});
+
+	}
+
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == PROJECT_SELECTION_REQUESTED) {
+			if (resultCode == RESULT_OK) {
+
+				String projID = mPrefs.getString("project_id", "");
+				if (!(projID.equals("") || projID.equals("-1"))) {
+					// TODO - got a project
+				} else {
+					// TODO - cancelled
+				}
+
+			}
+		} 
+	}
+
+}
