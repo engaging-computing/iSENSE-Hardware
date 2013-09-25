2a307b92329073ff488cef7449d004edb07bf5cf
stowellm <stowellmichael@gmail.com>
2013-09-25 10:41:07 -0400

    ProjectCreate gives user feedback, does the whole login cbeck thing, returns new proj ID

--- Android/Data_Collector/src/edu/uml/cs/isense/collector/splash/ProjectCreate.java
+++ Android/Data_Collector/src/edu/uml/cs/isense/collector/splash/ProjectCreate.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,8 +25,11 @@ import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import edu.uml.cs.isense.collector.R;
+import edu.uml.cs.isense.collector.dialogs.LoginActivity;
 import edu.uml.cs.isense.comm.API;
 import edu.uml.cs.isense.objects.RProjectField;
+import edu.uml.cs.isense.supplements.ObscuredSharedPreferences;
+import edu.uml.cs.isense.supplements.OrientationManager;
 import edu.uml.cs.isense.waffle.Waffle;
 
 public class ProjectCreate extends Activity {
@@ -35,11 +42,20 @@ public class ProjectCreate extends Activity {
 	private Spinner fieldSpin;
 	private LinearLayout fieldScroll;
 	private ScrollView fieldScrollHolder;
+	private EditText projectName;
+	
+	private ArrayList<RProjectField> fields;
+	
+	private int newProjID;
+	public static final String NEW_PROJECT_ID = "new_proj_id";
 	
 	private static final int FIELD_TYPE_TIMESTAMP 		= 0;
 	private static final int FIELD_TYPE_NUMBER	  		= 1;
 	private static final int FIELD_TYPE_TEXT			= 2;
 	private static final int FIELD_TYPE_LOCATION		= 3;
+	
+	private static final int LOGIN_REQUESTED = 100;
+	private ProgressDialog dia;
 	
 	private int locationCount   = 0;
 	
@@ -74,7 +90,7 @@ public class ProjectCreate extends Activity {
 			}
 		}
 		
-		final EditText projectName = (EditText) findViewById(R.id.project_create_name);
+		projectName = (EditText) findViewById(R.id.project_create_name);
 
 		// Set listeners for the buttons
 		final Button cancel = (Button) findViewById(R.id.project_create_cancel);
@@ -89,67 +105,7 @@ public class ProjectCreate extends Activity {
 		ok.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				
-				final ArrayList<RProjectField> fields = new ArrayList<RProjectField>();
-				
-				for (int i = 0; i < fieldScroll.getChildCount(); i++) {
-					View child = fieldScroll.getChildAt(i);
-					RProjectField field = new RProjectField();
-					EditText fieldName = (EditText) child.findViewById(R.id.project_field_name);
-					EditText units	   = (EditText) child.findViewById(R.id.project_field_units);
-					
-					switch ((Integer) child.getTag()) {
-					case FIELD_TYPE_TIMESTAMP:
-						field.name = fieldName.getText().toString();
-						field.type = RProjectField.TYPE_TIMESTAMP;
-						field.unit = "milliseconds"; // TODO - should there be milliseconds?
-						fields.add(field);
-						break;
-						
-					case FIELD_TYPE_NUMBER:
-						field.name = fieldName.getText().toString();
-						field.type = RProjectField.TYPE_NUMBER;
-						field.unit = units.getText().toString();
-						fields.add(field);
-						break;
-						
-					case FIELD_TYPE_TEXT:
-						field.name = fieldName.getText().toString();
-						field.type = RProjectField.TYPE_TEXT;
-						field.unit = "";	// TODO - should we even set to blank?
-						fields.add(field);
-						break;
-						
-					case FIELD_TYPE_LOCATION:
-						field.name = "Latitude";
-						field.type = RProjectField.TYPE_LAT;
-						field.unit = "deg";
-						fields.add(field);
-						
-						field = new RProjectField();
-						field.name = "Longitude";
-						field.type = RProjectField.TYPE_LON;
-						field.unit = "deg";
-						fields.add(field);
-
-						break;
-						
-					default:
-						break;
-					}
-				}
-				
-				Thread uploadThread = new Thread() {
-			        public void run(){
-			            try {
-			            	api.createSession("mobile", "mobile");
-							api.createProject(projectName.getText().toString(), fields); 
-			            } catch (Exception e) {
-			                e.printStackTrace();
-			            }
-			        }
-			    };
-			    uploadThread.start();
+				createProjectAfterCheckingLogin();
 			}
 		});
 		
@@ -257,6 +213,191 @@ public class ProjectCreate extends Activity {
 	        }
 	    };
 	    scrollThread.start();
+	}
+	
+	private void createProjectAfterCheckingLogin() {
+		
+		if (api.getCurrentUser() == null) {
+			new CheckLoginTask().execute();
+		} else {
+			createProject();
+		}
+		
+	}
+	
+	private void needLogin() {
+		w.make("Please login to iSENSE first", Waffle.LENGTH_SHORT, Waffle.IMAGE_WARN);
+		
+		Intent iLogin = new Intent(mContext, LoginActivity.class);
+		startActivityForResult(iLogin, LOGIN_REQUESTED);
+	}
+	
+	private void createProject() {
+		fields = new ArrayList<RProjectField>();
+		
+		for (int i = 0; i < fieldScroll.getChildCount(); i++) {
+			View child = fieldScroll.getChildAt(i);
+			RProjectField field = new RProjectField();
+			
+			EditText fieldName = (EditText) child.findViewById(R.id.project_field_name);
+			EditText units	   = (EditText) child.findViewById(R.id.project_field_units);
+			
+			switch ((Integer) child.getTag()) {
+			case FIELD_TYPE_TIMESTAMP:
+				field.name = fieldName.getText().toString();
+				field.type = RProjectField.TYPE_TIMESTAMP;
+				fields.add(field);
+				
+				break;
+				
+			case FIELD_TYPE_NUMBER:
+				field.name = fieldName.getText().toString();
+				field.type = RProjectField.TYPE_NUMBER;
+				field.unit = units.getText().toString();
+				fields.add(field);
+				
+				break;
+				
+			case FIELD_TYPE_TEXT:
+				field.name = fieldName.getText().toString();
+				field.type = RProjectField.TYPE_TEXT;
+				fields.add(field);
+				
+				break;
+				
+			case FIELD_TYPE_LOCATION:
+				field.name = "Latitude";
+				field.type = RProjectField.TYPE_LAT;
+				field.unit = "deg";
+				fields.add(field);
+				
+				field = new RProjectField();
+				field.name = "Longitude";
+				field.type = RProjectField.TYPE_LON;
+				field.unit = "deg";
+				fields.add(field);
+				
+				break;
+				
+			default:
+				break;
+			}
+		}
+		
+		new CreateProjectTask().execute();
+	}
+	
+	private class CheckLoginTask extends AsyncTask<Void, Integer, Void> {
+
+		boolean success = false;
+		
+		@Override
+		protected void onPreExecute() {
+
+			OrientationManager.disableRotation(ProjectCreate.this);
+
+			dia = new ProgressDialog(ProjectCreate.this);
+			dia.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			dia.setMessage("Trying to log in...");
+			dia.setCancelable(false);
+			dia.show();
+		}
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			
+			final SharedPreferences mPrefs = new ObscuredSharedPreferences(
+					ProjectCreate.mContext,
+					ProjectCreate.mContext.getSharedPreferences("USER_INFO",
+							Context.MODE_PRIVATE));
+
+			success = api.createSession(
+					mPrefs.getString("username", ""),
+					mPrefs.getString("password", ""));
+			
+			publishProgress(100);
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			
+			if (dia != null) dia.cancel();
+			OrientationManager.enableRotation(ProjectCreate.this);
+		
+			if (success) {
+				createProject();
+			} else {
+				needLogin();
+			}
+		
+		}
+	}
+	
+	private class CreateProjectTask extends AsyncTask<Void, Integer, Void> {
+		
+		@Override
+		protected void onPreExecute() {
+
+			OrientationManager.disableRotation(ProjectCreate.this);
+
+			dia = new ProgressDialog(ProjectCreate.this);
+			dia.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			dia.setMessage("Creating your new project...");
+			dia.setCancelable(false);
+			dia.show();
+		}
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			newProjID = api.createProject(projectName.getText().toString(), fields); 
+			
+			publishProgress(100);
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			if (dia != null) dia.cancel();
+			OrientationManager.enableRotation(ProjectCreate.this);
+			
+			if (newProjID != 0) {
+				
+				Intent iRet = new Intent();
+				iRet.putExtra(NEW_PROJECT_ID, newProjID);
+				setResult(RESULT_OK, iRet);
+				finish();
+				
+			} else {
+				// TODO - we failed
+			}
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == LOGIN_REQUESTED) {
+			if (resultCode == RESULT_OK) {
+				String returnCode = data.getStringExtra("returnCode");
+				if (returnCode.equals("Success")) {
+
+					w.make("Login successful", Waffle.LENGTH_LONG,
+							Waffle.IMAGE_CHECK);
+
+					createProject();
+
+				} else if (returnCode.equals("Failed")) {
+
+					Intent i = new Intent(mContext, LoginActivity.class);
+					startActivityForResult(i, LOGIN_REQUESTED);
+				} else {
+					// should never get here
+				}
+
+			} 
+		}
 	}
 
 	@Override
