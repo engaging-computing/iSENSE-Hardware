ef197ba522b6e142e14d8423a4e831549c14e9aa
stowellm <stowellmichael@gmail.com>
2013-10-11 13:03:17 -0400

    Added FieldMatching (albeit the user must do it once for every data set) to the queue saving dialog

--- Android/iSENSE Imports/src/edu/uml/cs/isense/queue/QueueLayout.java
+++ Android/iSENSE Imports/src/edu/uml/cs/isense/queue/QueueLayout.java
@@ -17,40 +17,43 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import edu.uml.cs.isense.R;
 import edu.uml.cs.isense.comm.API;
+import edu.uml.cs.isense.dfm.DataFieldManager;
+import edu.uml.cs.isense.dfm.FieldMatching;
+import edu.uml.cs.isense.dfm.Fields;
 import edu.uml.cs.isense.proj.Setup;
 import edu.uml.cs.isense.supplements.OrientationManager;
 import edu.uml.cs.isense.waffle.Waffle;
 
 /**
- * Activity that displays the list of data sets stored in the
- * data saving queue.  From here, the user can check and uncheck data
- * sets to upload, rename them, change their data, delete them,
- * or attempt to upload them to iSENSE.
+ * Activity that displays the list of data sets stored in the data saving queue.
+ * From here, the user can check and uncheck data sets to upload, rename them,
+ * change their data, delete them, or attempt to upload them to iSENSE.
  * 
  * @author Mike Stowell and Jeremy Poulin of the iSENSE team.
- *
+ * 
  */
 public class QueueLayout extends Activity implements OnClickListener {
 
 	/**
-	 * Global string constant that the user should use to pass in the 
-	 * parent name of their activity when using an intent to launch
-	 * QueueLayout.  QueueLayout will not display if a parent name is
-	 * not passed into it.  This parent name is used to create a 
-	 * serializable file with the same name as the string passed to it.
+	 * Global string constant that the user should use to pass in the parent
+	 * name of their activity when using an intent to launch QueueLayout.
+	 * QueueLayout will not display if a parent name is not passed into it. This
+	 * parent name is used to create a serializable file with the same name as
+	 * the string passed to it.
 	 */
 	public static final String PARENT_NAME = "parentName";
-	public static final String LOGIN_CONTEXT ="logincontext";
+	public static final String LOGIN_CONTEXT = "logincontext";
 
-	private static final int ALTER_DATASET_REQUESTED   		 = 9001;
-	private static final int ALTER_DATA_NAME_REQUESTED 		 = 9002;
-	private static final int ALTER_DATA_DATA_REQUESTED 	 	 = 9003;
-	private static final int ALTER_DATA_PROJ_REQUESTED 		 = 9004;
+	private static final int ALTER_DATASET_REQUESTED = 9001;
+	private static final int ALTER_DATA_NAME_REQUESTED = 9002;
+	private static final int ALTER_DATA_DATA_REQUESTED = 9003;
+	private static final int ALTER_DATA_PROJ_REQUESTED = 9004;
 	private static final int QUEUE_DELETE_SELECTED_REQUESTED = 9005;
-	
+	private static final int FIELD_MATCHING_REQUESTED = 9006;
+
 	private static final int QUEUE_BOX_DESELECTED = 0;
-	private static final int QUEUE_BOX_SELECTED   = 1;
-	
+	private static final int QUEUE_BOX_SELECTED = 1;
+
 	public static final String LAST_UPLOADED_DATA_SET_ID = "lastuploadeddatasetid";
 
 	private static int QUEUE_PARENT = -1;
@@ -68,7 +71,8 @@ public class QueueLayout extends Activity implements OnClickListener {
 	private View lastViewLongClicked;
 	private Waffle w;
 	private API api;
-	
+	private DataFieldManager dfm;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -97,10 +101,10 @@ public class QueueLayout extends Activity implements OnClickListener {
 
 		Button cancel = (Button) findViewById(R.id.cancel);
 		cancel.setOnClickListener(this);
-		
+
 		Button select = (Button) findViewById(R.id.select_deselect_all);
 		select.setOnClickListener(this);
-		
+
 		Button delete = (Button) findViewById(R.id.delete_selected);
 		delete.setOnClickListener(this);
 
@@ -139,7 +143,7 @@ public class QueueLayout extends Activity implements OnClickListener {
 			projIDText.setText("No Project");
 		else
 			projIDText.setText("Project: " + ds.getProjID());
-		
+
 		TextView uploadType = (TextView) view.findViewById(R.id.upload_type);
 		uploadType.setText("Type: " + ds.getType());
 
@@ -150,18 +154,19 @@ public class QueueLayout extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		int id = v.getId();
 		if (id == R.id.upload) {
-			
+
 			if (allSelectedDataSetsHaveProjects()) {
 				if (!api.hasConnectivity()) {
 					w.make("No internet connection found", Waffle.IMAGE_X);
 					return;
 				}
-				
+
 				if (api.getCurrentUser() == null) {
-					w.make("Login information not found - please login again", Waffle.IMAGE_X);
+					w.make("Login information not found - please login again",
+							Waffle.IMAGE_X);
 					return;
 				}
-				
+
 				lastSID = -1;
 				if (uq.mirrorQueue.isEmpty()) {
 					uq.storeAndReRetrieveQueue(true);
@@ -169,20 +174,23 @@ public class QueueLayout extends Activity implements OnClickListener {
 					return;
 				} else {
 					new UploadSDTask().execute();
-					// clear the queue so we can re-add un-uploaded data sets from the mirrorQueue
+					// clear the queue so we can re-add un-uploaded data sets
+					// from the mirrorQueue
 					uq.queue = new LinkedList<QDataSet>();
 				}
 			} else {
-				Intent iNoInitialProject = new Intent(QueueLayout.this, NoInitialProject.class);
+				Intent iNoInitialProject = new Intent(QueueLayout.this,
+						NoInitialProject.class);
 				startActivity(iNoInitialProject);
 			}
-				
+
 		} else if (id == R.id.cancel) {
 			setResultAndFinish(RESULT_CANCELED);
 			finish();
 		} else if (id == R.id.select_deselect_all) {
 			Button select = (Button) findViewById(R.id.select_deselect_all);
-			if (select.getText().equals(getResources().getString(R.string.select_all))) {
+			if (select.getText().equals(
+					getResources().getString(R.string.select_all))) {
 				select.setText(getResources().getString(R.string.deselect_all));
 				for (int i = 0; i < scrollQueue.getChildCount(); i++) {
 					View view = scrollQueue.getChildAt(i);
@@ -199,17 +207,21 @@ public class QueueLayout extends Activity implements OnClickListener {
 					}
 				}
 			}
-			
+
 		} else if (id == R.id.delete_selected) {
-			Intent iDelSel = new Intent(QueueLayout.this, QueueDeleteSelected.class);
+			Intent iDelSel = new Intent(QueueLayout.this,
+					QueueDeleteSelected.class);
 			startActivityForResult(iDelSel, QUEUE_DELETE_SELECTED_REQUESTED);
 		}
-		
+
 	}
-	
+
 	private boolean allSelectedDataSetsHaveProjects() {
 		for (QDataSet qds : uq.queue) {
-			if (qds.isUploadable() && (qds.getProjID().equals("-1") || qds.getProjID().equals("No Proj.")|| qds.getProjID().equals("No Project"))) {
+			if (qds.isUploadable()
+					&& (qds.getProjID().equals("-1")
+							|| qds.getProjID().equals("No Proj.") || qds
+							.getProjID().equals("No Project"))) {
 				return false;
 			}
 		}
@@ -243,14 +255,15 @@ public class QueueLayout extends Activity implements OnClickListener {
 			uploadSet = uq.mirrorQueue.remove();
 			if (!uploadSet.isUploadable())
 				doThings = false;
-			
+
 			createRunnable(uploadSet);
 
 			OrientationManager.disableRotation(QueueLayout.this);
 
 			dia = new ProgressDialog(QueueLayout.this);
 			dia.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-			dia.setMessage("Please wait while \"" + uploadSet.getName() + "\" is uploaded...");
+			dia.setMessage("Please wait while \"" + uploadSet.getName()
+					+ "\" is uploaded...");
 			dia.setCancelable(false);
 			try {
 				dia.show();
@@ -265,10 +278,10 @@ public class QueueLayout extends Activity implements OnClickListener {
 		protected Void doInBackground(Void... voids) {
 			if (doThings)
 				sdUploader.run();
-			
+
 			if (dia != null)
 				dia.setProgress(100);
-			
+
 			return null;
 		}
 
@@ -277,18 +290,21 @@ public class QueueLayout extends Activity implements OnClickListener {
 			Waffle w = new Waffle(mContext);
 
 			if (!doThings) {
-				w.make("\"" + uploadSet.getName() + "\" chosen not to upload", Waffle.LENGTH_SHORT, Waffle.IMAGE_WARN);
+				w.make("\"" + uploadSet.getName() + "\" chosen not to upload",
+						Waffle.LENGTH_SHORT, Waffle.IMAGE_WARN);
 				uq.queue.add(uploadSet);
 				uq.storeAndReRetrieveQueue(false);
 			} else if (dataSetID != -1)
-				w.make("Upload success for \"" + uploadSet.getName() + "\"", Waffle.LENGTH_SHORT,
-						Waffle.IMAGE_CHECK);
+				w.make("Upload success for \"" + uploadSet.getName() + "\"",
+						Waffle.LENGTH_SHORT, Waffle.IMAGE_CHECK);
 			else if (uploadSet.getProjID().equals("-1")) {
-				w.make("Select a project first for \"" + uploadSet.getName() + "\"", Waffle.LENGTH_LONG, Waffle.IMAGE_X);
+				w.make("Select a project first for \"" + uploadSet.getName()
+						+ "\"", Waffle.LENGTH_LONG, Waffle.IMAGE_X);
 				uq.queue.add(uploadSet);
 				uq.storeAndReRetrieveQueue(false);
 			} else {
-				w.make("Upload failed - project is closed, deleted, or contains broken data sets", Waffle.LENGTH_LONG, Waffle.IMAGE_X);
+				w.make("Upload failed - project is closed, deleted, or contains broken data sets",
+						Waffle.LENGTH_LONG, Waffle.IMAGE_X);
 				uq.queue.add(uploadSet);
 				uq.storeAndReRetrieveQueue(false);
 			}
@@ -301,10 +317,10 @@ public class QueueLayout extends Activity implements OnClickListener {
 				setResultAndFinish(RESULT_OK);
 				return;
 			} else {
-				//if (uploadSuccess)
+				// if (uploadSuccess)
 				continueUploading();
 			}
-			
+
 			OrientationManager.enableRotation(QueueLayout.this);
 		}
 	}
@@ -339,11 +355,11 @@ public class QueueLayout extends Activity implements OnClickListener {
 			if (resultCode == RESULT_OK) {
 
 				int returnCode = data.getIntExtra(QueueAlter.RETURN_CODE, -1);
-				
+
 				switch (returnCode) {
 
 				case QueueAlter.RENAME:
-					
+
 					Intent iRename = new Intent(mContext, QueueEditRename.class);
 					startActivityForResult(iRename, ALTER_DATA_NAME_REQUESTED);
 
@@ -355,17 +371,21 @@ public class QueueLayout extends Activity implements OnClickListener {
 					startActivityForResult(iData, ALTER_DATA_DATA_REQUESTED);
 
 					break;
-					
+
 				case QueueAlter.SELECT_PROJECT:
-					
-					Intent iProj = new Intent(mContext, Setup.class);
-					iProj.putExtra("from_where", "queue");
-					startActivityForResult(iProj, ALTER_DATA_PROJ_REQUESTED);
-					
+
+					if (api.hasConnectivity()) {
+						Intent iProj = new Intent(mContext, Setup.class);
+						iProj.putExtra("from_where", "queue");
+						startActivityForResult(iProj, ALTER_DATA_PROJ_REQUESTED);
+					} else
+						w.make("You need internet connectivity to select a project",
+								Waffle.LENGTH_LONG, Waffle.IMAGE_X);
+
 					break;
 
 				case QueueAlter.DELETE:
-					
+
 					uq.removeItemWithKey(lastDataSetLongClicked.key);
 					scrollQueue.removeView(lastViewLongClicked);
 
@@ -396,27 +416,17 @@ public class QueueLayout extends Activity implements OnClickListener {
 			if (resultCode == RESULT_OK) {
 
 				QDataSet alter = QueueEditData.alter;
-			
+
 				uq.removeItemWithKey(lastDataSetLongClicked.key);
 				scrollQueue.removeView(lastViewLongClicked);
-				
+
 				uq.addDataSetToQueue(alter);
 				addViewToScrollQueue(alter);
-				
+
 			}
 		} else if (requestCode == ALTER_DATA_PROJ_REQUESTED) {
 			if (resultCode == RESULT_OK) {
-				SharedPreferences mPrefs = getSharedPreferences("PROJID_QUEUE", 0);
-				
-				QDataSet alter = lastDataSetLongClicked;
-				alter.setProj(mPrefs.getString("project_id", "No Proj."));
-				
-				uq.removeItemWithKey(lastDataSetLongClicked.key);
-				scrollQueue.removeView(lastViewLongClicked);
-				
-				uq.addDataSetToQueue(alter);
-				addViewToScrollQueue(alter);
-				
+				new PrepForFieldMatchTask().execute();
 			}
 		} else if (requestCode == QUEUE_DELETE_SELECTED_REQUESTED) {
 			if (resultCode == RESULT_OK) {
@@ -425,26 +435,107 @@ public class QueueLayout extends Activity implements OnClickListener {
 				for (int i = 0; i < count; i++) {
 					View view = scrollQueue.getChildAt(childIndex);
 					if (view.getTag() == Integer.valueOf(QUEUE_BOX_SELECTED)) {
-						long dataSetKey = Long.parseLong("" + view.getContentDescription());
+						long dataSetKey = Long.parseLong(""
+								+ view.getContentDescription());
 						uq.removeItemWithKey(dataSetKey);
 						scrollQueue.removeView(view);
 					} else
 						++childIndex;
 				}
 			}
+		} else if (requestCode == FIELD_MATCHING_REQUESTED) {
+			if (resultCode == RESULT_OK) {
+				if (FieldMatching.acceptedFields.isEmpty()) {
+					Intent iProj = new Intent(mContext, Setup.class);
+					iProj.putExtra("from_where", "queue");
+					startActivityForResult(iProj, ALTER_DATA_PROJ_REQUESTED);
+				} else if (!FieldMatching.compatible) {
+					Intent iProj = new Intent(mContext, Setup.class);
+					iProj.putExtra("from_where", "queue");
+					startActivityForResult(iProj, ALTER_DATA_PROJ_REQUESTED);
+				} else {
+					SharedPreferences mPrefs = getSharedPreferences("PROJID_QUEUE", 0);
+					
+					QDataSet alter = lastDataSetLongClicked;
+					alter.setProj(mPrefs.getString("project_id", "No Proj."));
+					alter.setFields(FieldMatching.acceptedFields);
+
+					uq.removeItemWithKey(lastDataSetLongClicked.key);
+					scrollQueue.removeView(lastViewLongClicked);
+
+					uq.addDataSetToQueue(alter);
+					addViewToScrollQueue(alter);
+
+				}
+			} else if (resultCode == RESULT_CANCELED) {
+				Intent iProj = new Intent(mContext, Setup.class);
+				iProj.putExtra("from_where", "queue");
+				startActivityForResult(iProj, ALTER_DATA_PROJ_REQUESTED);
+			}	
 		}
 
 	}
 
+	// Task for getting dfm's order array before calling the FieldMatching dialog
+	private class PrepForFieldMatchTask extends AsyncTask<Void, Integer, Void> {
+
+		private ProgressDialog dia;
+
+		@Override
+		protected void onPreExecute() {
+			OrientationManager.disableRotation(QueueLayout.this);
+
+			dia = new ProgressDialog(QueueLayout.this);
+			dia.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			dia.setMessage("Gathering project fields...");
+			dia.setCancelable(false);
+			dia.show();
+		}
+
+		@Override
+		protected Void doInBackground(Void... voids) {
+
+			SharedPreferences mPrefs = getSharedPreferences("PROJID_QUEUE", 0);
+			String projectInput = mPrefs.getString("project_id", "");
+
+			Fields f = new Fields();
+			dfm = new DataFieldManager(Integer.parseInt(projectInput), api,
+					mContext, f);
+			dfm.getOrderWithExternalAsyncTask();
+			dfm.writeProjectFields();
+
+			publishProgress(100);
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void voids) {
+			dia.setMessage("Done");
+			dia.cancel();
+
+			OrientationManager.enableRotation(QueueLayout.this);
+
+			Intent iFieldMatch = new Intent(mContext, FieldMatching.class);
+
+			String[] dfmOrderList = dfm.convertOrderToStringArray();
+
+			iFieldMatch.putExtra(FieldMatching.DFM_ORDER_LIST, dfmOrderList);
+			startActivityForResult(iFieldMatch, FIELD_MATCHING_REQUESTED);
+		}
+	}
+
 	// Adds ds to the scrollQueue object
-	// Each block in the scrollQueue gets 2 additional (and confusing) properties:
-	//		tag: the selection state of the queue block (QUEUE_BOX_SELECTED or QUEUE_BOX_DESELECTED)
-	//		content description: the key of the associated data set
+	// Each block in the scrollQueue gets 2 additional (and confusing)
+	// properties:
+	// tag: the selection state of the queue block (QUEUE_BOX_SELECTED or
+	// QUEUE_BOX_DESELECTED)
+	// content description: the key of the associated data set
 	private void addViewToScrollQueue(final QDataSet ds) {
 
 		String previous = "";
 		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-			     LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+				LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
 
 		layoutParams.setMargins(5, 5, 5, 5);
 
@@ -461,15 +552,15 @@ public class QueueLayout extends Activity implements OnClickListener {
 				data.setBackgroundResource(R.drawable.listelement_bkgd_changer);
 				data.setTag(QUEUE_BOX_DESELECTED);
 			}
-			
+
 			makeBlock(data, ds);
 			previous = checkPrevious(previous, scrollQueue,
 					(String) ds.getName());
 
 			scrollQueue.addView(data, layoutParams);
-			//ds.setUploadable(true);
+			// ds.setUploadable(true);
 			data.setContentDescription("" + ds.key);
-			
+
 			data.setOnClickListener(new OnClickListener() {
 
 				public void onClick(View v) {
@@ -492,12 +583,16 @@ public class QueueLayout extends Activity implements OnClickListener {
 				public boolean onLongClick(View v) {
 					lastDataSetLongClicked = ds;
 					lastViewLongClicked = data;
-					boolean isFromDataCollector = (parentName.equals("datacollector")) || (parentName.equals("carrampphysics"))
+					boolean isFromDataCollector = (parentName
+							.equals("datacollector"))
+							|| (parentName.equals("carrampphysics"))
 							|| (parentName.equals("data_walk"));
 					Intent iAlterDataSet = new Intent(mContext,
 							QueueAlter.class);
-					iAlterDataSet.putExtra(QueueAlter.IS_ALTERABLE, !isFromDataCollector);
-					iAlterDataSet.putExtra(QueueAlter.SELECT_PROJ, !lastDataSetLongClicked.getHasInitialProject());
+					iAlterDataSet.putExtra(QueueAlter.IS_ALTERABLE,
+							!isFromDataCollector);
+					iAlterDataSet.putExtra(QueueAlter.SELECT_PROJ,
+							!lastDataSetLongClicked.getHasInitialProject());
 					startActivityForResult(iAlterDataSet,
 							ALTER_DATASET_REQUESTED);
 					return false;
@@ -525,7 +620,7 @@ public class QueueLayout extends Activity implements OnClickListener {
 					(String) ds.getName());
 
 			scrollQueue.addView(pic, layoutParams);
-			//ds.setUploadable(true);
+			// ds.setUploadable(true);
 			pic.setContentDescription("" + ds.key);
 			pic.setTag(QUEUE_BOX_SELECTED);
 
@@ -564,9 +659,9 @@ public class QueueLayout extends Activity implements OnClickListener {
 			});
 
 			break;
-			
+
 		case BOTH:
-			
+
 			final View both = View.inflate(mContext, R.layout.queueblock_pic,
 					null);
 
@@ -583,7 +678,7 @@ public class QueueLayout extends Activity implements OnClickListener {
 					(String) ds.getName());
 
 			scrollQueue.addView(both, layoutParams);
-			//ds.setUploadable(true);
+			// ds.setUploadable(true);
 			both.setContentDescription("" + ds.key);
 			both.setTag(QUEUE_BOX_SELECTED);
 
@@ -612,7 +707,8 @@ public class QueueLayout extends Activity implements OnClickListener {
 					Intent iAlterDataSet = new Intent(mContext,
 							QueueAlter.class);
 					iAlterDataSet.putExtra(QueueAlter.IS_ALTERABLE, false);
-					iAlterDataSet.putExtra(QueueAlter.SELECT_PROJ, lastDataSetLongClicked.getProjID().equals("-1"));
+					iAlterDataSet.putExtra(QueueAlter.SELECT_PROJ,
+							lastDataSetLongClicked.getProjID().equals("-1"));
 					iAlterDataSet.putExtra("parent", QUEUE_PARENT);
 					startActivityForResult(iAlterDataSet,
 							ALTER_DATASET_REQUESTED);
@@ -620,7 +716,7 @@ public class QueueLayout extends Activity implements OnClickListener {
 				}
 
 			});
-			
+
 			break;
 		}
 
