84c2a4096b7f410545f9b4ed8f97d5a35e4fe104
bdonald25 <bobbydonald25@gmail.com>
2013-11-18 11:56:19 -0500

    fix merge issue

--- Android/River_Walk_Pictures/src/edu/uml/cs/isense/riverwalk/Main.java
+++ Android/River_Walk_Pictures/src/edu/uml/cs/isense/riverwalk/Main.java
@@ -38,7 +38,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
