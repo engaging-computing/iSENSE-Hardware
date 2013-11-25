8279ae98be3526094ab12724bdec9287896cd47f
bdonald25 <bobbydonald25@gmail.com>
2013-11-25 15:01:38 -0500

    black preview
    Former-commit-id: 7d584b3d258e354949f764a4aa337c1dcf4e3f99

--- River_Walk_Pictures/src/edu/uml/cs/isense/riverwalk/dialogs/CameraPreview.java
+++ River_Walk_Pictures/src/edu/uml/cs/isense/riverwalk/dialogs/CameraPreview.java
@@ -24,12 +24,16 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         mHolder.addCallback(this);
         // deprecated setting, but required on Android versions prior to 3.0
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        
     }
 
+    @Override
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, now tell the camera where to draw the preview.
+    	 
     	
-        
+    	if (holder == null);
+    	Log.d("surfaceCreated", "holder is null");
         try {
             if (mCamera != null) {
                 mCamera.setPreviewDisplay(holder);
@@ -41,12 +45,14 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     }
 
+    @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
     	mCamera.stopPreview();
 		mCamera.release();
 		mCamera = null;
     }
 
+    @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
@@ -62,16 +68,18 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         } catch (Exception e){
           // ignore: tried to stop a non-existent preview
         }
-        Log.d("CameraPreview", "Camera is:" + mCamera.toString());
-        Camera.Parameters parameters = mCamera.getParameters();
-        List<Camera.Size> sizeList = parameters.getSupportedPreviewSizes();
-        int height = sizeList.get(0).height;
-        int width = sizeList.get(0).width;
-        parameters.setPreviewSize(width, height);
-        mCamera.setParameters(parameters);
+//        Log.d("CameraPreview", "Camera is:" + mCamera.toString());
+//        Camera.Parameters parameters = mCamera.getParameters();
+//        List<Camera.Size> sizeList = parameters.getSupportedPreviewSizes();
+//        int height = sizeList.get(0).height;
+//        int width = sizeList.get(0).width;
+//        parameters.setPreviewSize(width, height);
+//        mCamera.setParameters(parameters);
+//        holder.setFixedSize(width, height);
         // set preview size and make any resize, rotate or
         // reformatting changes here
-
+       
+        
         // start preview with new settings
         try {
             mCamera.setPreviewDisplay(mHolder);
