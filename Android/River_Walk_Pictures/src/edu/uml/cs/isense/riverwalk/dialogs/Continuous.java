ba9f0f8e434d9fbebc78fe4d73535f090cb2feba
bdonald25 <bobbydonald25@gmail.com>
2013-12-04 14:00:41 -0500

    no longer can set 0 as an interval, also can no longer leave interval blank

--- Android/River_Walk_Pictures/src/edu/uml/cs/isense/riverwalk/dialogs/Continuous.java
+++ Android/River_Walk_Pictures/src/edu/uml/cs/isense/riverwalk/dialogs/Continuous.java
@@ -57,8 +57,14 @@ public class Continuous extends Activity {
 	ok.setOnClickListener(new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			Main.continuousInterval = Integer.parseInt(continuous_time.getText().toString());
-			finish();
+			try{
+				Main.continuousInterval = Integer.parseInt(continuous_time.getText().toString());
+				if (Main.continuousInterval == 0)
+					Main.continuousInterval = 1;
+				finish();
+			} catch(NumberFormatException e) {
+				continuous_time.setError("Please Enter a Value.");
+			}
 		}
 	});	
 	
