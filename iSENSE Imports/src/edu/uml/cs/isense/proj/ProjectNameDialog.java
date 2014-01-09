c95cc747dc62c783b751080e6e22c4ec35e575b1
Virinchi Balabhadrapatruni <Virinchi_Balabhadrapatruni@student.uml.edu>
2014-01-09 09:28:09 -0500

    API and CRP Changes concerning setup of projects. Data Walk can also implement project creation with constricted fields if Rajia wants
    Former-commit-id: 598b990708e9bd98a93ac249fe8dd659c61f0cca

--- iSENSE Imports/src/edu/uml/cs/isense/proj/ProjectNameDialog.java
+++ iSENSE Imports/src/edu/uml/cs/isense/proj/ProjectNameDialog.java
@@ -10,27 +10,27 @@ import android.widget.EditText;
 
 public class ProjectNameDialog extends Activity {
 	
-	private EditText input;
+	private EditText nameInput;
 	private Button ok,cancel;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_project_name);
 		
-		input = (EditText) findViewById(R.id.new_proj_name);
+		nameInput = (EditText) findViewById(R.id.new_proj_name);
 		ok = (Button) findViewById(R.id.okB);
 		cancel = (Button) findViewById(R.id.clB);
 		
 		ok.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
-				String x = input.getText().toString();
-				if (x.equals("")){
-					input.setError("Project Name Cannot Be Empty");
+				String name = nameInput.getText().toString();
+				if (name.equals("")){
+					nameInput.setError("Project Name Cannot Be Empty");
 				} else {
-					input.setError(null);
+					nameInput.setError(null);
 					Intent data = new Intent();
-					data.putExtra("new_proj_name", x);
+					data.putExtra("new_proj_name", name);
 					setResult(RESULT_OK, data);
 					finish();
 				}
