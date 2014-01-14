ee49239d6911ea4705f31b1f37943074ef69a486
rajia A <rajia_abdelaziz@student.uml.edu>
2014-01-14 13:02:50 -0500

    A service class created and the Framework for a service has been created, permissions added in Manifest
    Former-commit-id: bd7d03b4999e915f6d291c88a4eed3a53d89d9f0

---
+++ DataWalk_v2/src/edu/uml/cs/isense/datawalk_v2/Datawalk_Service.java
@@ -0,0 +1,30 @@
+package edu.uml.cs.isense.datawalk_v2;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class Datawalk_Service extends Service {
+private static final String TAG	= Datawalk_Service.class.getSimpleName();
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		Log.d("TAG", "onCreate'd");
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.d("TAG", "onDestroy'd");
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.d("TAG", "onBind'd");
+		return null;
+	}
+
+}
