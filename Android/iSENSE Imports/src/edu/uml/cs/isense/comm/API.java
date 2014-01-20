8a757b1de1ce608482ff48d730648ed93e9eaad3
jaypoulz <jaypoulz@gmail.com>
2014-01-20 13:16:27 -0500

    Update version string

--- Android/iSENSE Imports/src/edu/uml/cs/isense/comm/API.java
+++ Android/iSENSE Imports/src/edu/uml/cs/isense/comm/API.java
@@ -49,7 +49,7 @@ import edu.uml.cs.isense.objects.RTutorial;
 
 public class API {
 	private String version_major = "4";
-	private String version_minor = "0";
+	private String version_minor = "1";
 	private String version;
 
 	private static API instance = null;
