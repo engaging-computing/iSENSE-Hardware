ffbb53ddd28b0762dfd6c0c5863e2b58896482eb
bdonald25 <bobbydonald25@gmail.com>
2014-03-03 17:13:22 -0500

    working on implementing login
    Former-commit-id: d2ab6816cf816c102ab80ffc5f5104de5559e52d

--- iSENSE Imports/src/edu/uml/cs/isense/credentials/CredentialManagerKeys.java
+++ iSENSE Imports/src/edu/uml/cs/isense/credentials/CredentialManagerKeys.java
@@ -8,26 +8,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class CredentialManagerKeys extends Fragment {
-	@Override
-    public void onActivityCreated(Bundle savedInstanceState)
-    {
-        super.onActivityCreated(savedInstanceState);
-         
-    }
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.credential_manager_keys, container, false);
-    }
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
 
 
-	
-    
+	 
 	
 }
 
