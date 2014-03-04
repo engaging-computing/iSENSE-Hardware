be9fa7431eda6ccc4bb734ce344ef9836a488373
bdonald25 <bobbydonald25@gmail.com>
2014-03-04 15:43:51 -0500

    working on getting login function working

--- Android/iSENSE Imports/src/edu/uml/cs/isense/credentials/CredentialManagerPerson.java
+++ Android/iSENSE Imports/src/edu/uml/cs/isense/credentials/CredentialManagerPerson.java
@@ -8,15 +8,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class CredentialManagerPerson extends Fragment {
-
+	ImageView gravatar;
+	TextView email;
+	
+	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.credential_manager_person, container, false);
+        View view =  inflater.inflate(R.layout.credential_manager_person, container, false);
+        
+        gravatar = (ImageView) view.findViewById(R.id.imageViewPerson);
+        Button logout = (Button) view.findViewById(R.id.buttonLogout);
+        email = (TextView) view.findViewById(R.id.textViewPersonName);
+        
+        email.setText(CredentialManager.person.person_id);
+        
+        return view;
     }
 
 	@Override
