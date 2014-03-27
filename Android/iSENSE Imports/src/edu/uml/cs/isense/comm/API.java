9e96b017b5b450b6c1ef2f3eb57efe005a6b3e1a
bdonald25 <bobbydonald25@gmail.com>
2014-03-27 13:59:32 -0400

    but fix for datasets having the wrong name with credential key

--- Android/iSENSE Imports/src/edu/uml/cs/isense/comm/API.java
+++ Android/iSENSE Imports/src/edu/uml/cs/isense/comm/API.java
@@ -424,7 +424,7 @@ public class API {
 	 * @param conKey
 	 *            The Contributor Key
 	 * @param conName
-	 *            The Contributor name
+	 *            The Contributor name          
 	 * @return The integer ID of the newly uploaded dataset, or -1 if upload
 	 *         fails
 	 */
@@ -435,7 +435,7 @@ public class API {
 			requestData.put("contribution_key", conKey);
 			requestData.put("contributor_name", conName);
 			requestData.put("data", data);
-			requestData.put("title", "title");
+			requestData.put("title", conName + appendedTimeStamp()); //dataset name is conName + timestamp
 			String reqResult = makeRequest(
 					baseURL,
 					"projects/" + projectId + "/jsonDataUpload", "", "POST",
@@ -628,7 +628,7 @@ public class API {
 					"upload",
 					new FileBody(mediaToUpload, URLConnection
 							.guessContentTypeFromName(mediaToUpload.getName())));
-			entity.addPart("contributor_key", new StringBody(conKey));
+			entity.addPart("contribution_key", new StringBody(conKey));
 			entity.addPart("contributor_name", new StringBody(conName));
 			entity.addPart("type", new StringBody((ttype == TargetType.PROJECT) ? "project" : "data_set"));
 			entity.addPart("id", new StringBody(""+projectId));
