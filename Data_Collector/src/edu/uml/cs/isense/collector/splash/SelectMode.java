3efb204c04e5aa70f187e6fd017d9bff65b67581
bdonald25 <bobbydonald25@gmail.com>
2014-04-09 14:52:35 -0400

    data collector now has new credential manager and no longer uses csv files
    Former-commit-id: 62c61058bd6f5acf7b6de53acf89fca0d0df8f6a

--- Data_Collector/src/edu/uml/cs/isense/collector/splash/SelectMode.java
+++ Data_Collector/src/edu/uml/cs/isense/collector/splash/SelectMode.java
@@ -1,19 +1,14 @@
 package edu.uml.cs.isense.collector.splash;
 
-import java.io.File;
-import java.util.Locale;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.MenuItem;
@@ -24,26 +19,16 @@ import android.widget.TextView;
 import edu.uml.cs.isense.collector.DataCollector;
 import edu.uml.cs.isense.collector.ManualEntry;
 import edu.uml.cs.isense.collector.R;
-import edu.uml.cs.isense.comm.API;
-import edu.uml.cs.isense.comm.Connection;
-import edu.uml.cs.isense.credentials.CredentialManager;
-import edu.uml.cs.isense.credentials.Login;
-import edu.uml.cs.isense.supplements.FileBrowser;
-import edu.uml.cs.isense.supplements.ObscuredSharedPreferences;
-import edu.uml.cs.isense.supplements.OrientationManager;
-import edu.uml.cs.isense.waffle.Waffle;
+
 
 public class SelectMode extends Activity {
 
 	private static Context mContext;
-	private Waffle w;
-	private API api;
 
-	private String tempFilepath = "";
 
-	public static final String ENABLE_MANUAL_AND_CSV = "enable_manual_and_csv";
 
-	private static final int UPLOAD_CSV_REQUESTED = 100;
+	public static final String ENABLE_MANUAL = "enable_manual";
+
 	private static final int LOGIN_REQUESTED = 101;
 
 	@SuppressLint("NewApi")
@@ -53,8 +38,7 @@ public class SelectMode extends Activity {
 		setContentView(R.layout.select_mode);
 
 		mContext = this;
-		w = new Waffle(mContext);
-		api = API.getInstance();
+
 
 		// Action bar customization for API >= 14
 		if (android.os.Build.VERSION.SDK_INT >= 14) {
@@ -101,43 +85,19 @@ public class SelectMode extends Activity {
 				+ "<font COLOR=\"#D9A414\">" + "(requires project)" + "</font>";
 		manualEntry.setText(Html.fromHtml(manualEntryText));
 
-		final Button csvUploader = (Button) findViewById(R.id.select_mode_csv_uploader);
-		csvUploader.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (!Connection.hasConnectivity(mContext))
-					w.make(getResources().getString(R.string.need_connectivity_to_do),
-							Waffle.LENGTH_LONG, Waffle.IMAGE_WARN);
-				else {
-					Intent iFileBrowse = new Intent(mContext, FileBrowser.class);
-					startActivityForResult(iFileBrowse, UPLOAD_CSV_REQUESTED);
-				}
-				
-			}
-		});
-		String csvUploaderText = "<font COLOR=\"#0066FF\">"
-				+ "Upload a .csv File From My Device" + "</font>" + "<br/>"
-				+ "<font COLOR=\"#D9A414\">"
-				+ "(requires project)" + "</font>";
-		csvUploader.setText(Html.fromHtml(csvUploaderText));
+		
+		
 
-		// Determine if we should disable manual entry and .csv uploader
+		// Determine if we should disable manual entry 
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-			boolean en = extras.getBoolean(ENABLE_MANUAL_AND_CSV);
+			boolean en = extras.getBoolean(ENABLE_MANUAL);
 			if (!en) {
 				manualEntry.setEnabled(false);
 				String m = "<font COLOR=\"#0066FF\">" + "Manually Enter Data"
 						+ "</font>" + "<br/>" + "<font COLOR=\"#B88804\">"
 						+ "(requires project)" + "</font>";
 				manualEntry.setText(Html.fromHtml(m));
-
-				csvUploader.setEnabled(false);
-				String c = "<font COLOR=\"#0066FF\">"
-						+ "Upload a .csv File From My Device" + "</font>"
-						+ "<br/>" + "<font COLOR=\"#B88804\">"
-						+ "(requires project)" + "</font>";
-				csvUploader.setText(Html.fromHtml(c));
 			}
 		}
 
@@ -147,100 +107,14 @@ public class SelectMode extends Activity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
-		if (requestCode == UPLOAD_CSV_REQUESTED) {
+	if (requestCode == LOGIN_REQUESTED) {
 			if (resultCode == RESULT_OK) {
-				String filepath = data.getStringExtra("filepath");
-				if (filepath.length() == 0) {
-					w.make(getResources().getString(R.string.could_not_find_csv),
-							Waffle.LENGTH_SHORT,
-							Waffle.IMAGE_X);
-					return;
-				}
-
-				if (!Connection.hasConnectivity(mContext)) {
-					w.make(getResources().getString(R.string.no_csv_no_internet),
-							Waffle.LENGTH_SHORT, Waffle.IMAGE_WARN);
-					return;
-				}
-
-				String[] comp = filepath.split("\\.");
-
-				if (comp.length == 0
-						|| !(comp[comp.length - 1].toLowerCase(Locale.US)
-								.equals("csv"))) {
-					w.make(getResources().getString(R.string.only_csv_files),
-							Waffle.LENGTH_SHORT, Waffle.IMAGE_WARN);
-					return;
-				}
-
-				CredentialManager.Login(mContext, api);
-				new UploadCSVTask().execute(filepath);	
-			}
-		} else if (requestCode == LOGIN_REQUESTED) {
-			if (resultCode == RESULT_OK) {
-				new UploadCSVTask().execute(tempFilepath);
 			} 
 		}
 	}
 
 	
 
-	private class UploadCSVTask extends AsyncTask<String, Integer, Void> {
-
-		private ProgressDialog dia;
-		private int dsid = -1;
-
-		@Override
-		protected void onPreExecute() {
-
-			OrientationManager.disableRotation(SelectMode.this);
-
-			dia = new ProgressDialog(SelectMode.this);
-			dia.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-			dia.setMessage("Please wait while the .csv file is uploaded...");
-			dia.setCancelable(false);
-			dia.show();
-		}
-
-		@Override
-		protected Void doInBackground(String... args) {
-
-			SharedPreferences globalProjPrefs = getSharedPreferences(
-					"GLOBAL_PROJ", 0);
-			int projectId = Integer.parseInt(globalProjPrefs.getString(
-					"project_id_csv", "-1"));
-
-			File csvToUpload = new File(args[0]);
-
-			String[] components = args[0].split("/");
-			String datasetName = components[components.length - 1];
-
-			dsid = api.uploadCSV(projectId, csvToUpload, datasetName);
-
-			publishProgress(100);
-			return null;
-
-		}
-
-		@Override
-		protected void onPostExecute(Void voids) {
-			dia.setMessage("Done");
-			dia.cancel();
-			OrientationManager.enableRotation(SelectMode.this);
-
-			if (dsid <= 0) {
-				w.make(getResources().getString(R.string.csv_failed_upload), 
-						Waffle.LENGTH_SHORT,
-						Waffle.IMAGE_X);
-			} else {
-				w.make(getResources().getString(R.string.csv_success_upload), 
-						Waffle.LENGTH_SHORT,
-						Waffle.IMAGE_CHECK);
-			}
-
-		}
-
-	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
